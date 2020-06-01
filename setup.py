6d1bf4d9ef5fbdba355487bd58754bc3fbabd53a
Bill Back <bill.back@thoughtspot.com>
2020-06-01 13:26:00 -0700

    Initial setup.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-tql',
+    version='1.0',
+    python_requires='>3.6',
+    description='This package contains tools and libraries for working with ThoughtSpot by CS users.',
+    long_description_content_type="text/markdown",
+    url='https://thoughtspot.com',
+    author='Bill Back',
+    author_email='bill.back@thoughtspot.com',
+    license='MIT',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+    ]
+)
