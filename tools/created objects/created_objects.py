eeb757163ad2f48136be96aa722eaa0c7bd71914
Nick Cooper <nicholas.cooper@thoughtspot.com>
2020-08-18 11:48:24 -0500

    releasing v1.0.0 of created_objects

--- tools/created objects/created_objects.py
+++ tools/created objects/created_objects.py
@@ -1,8 +1,4 @@
-"""
-Find all objects in your ThoughtSpot platform of a specified type, and
-return their dependents.
-"""
-from typing import List, Dict, Any
+from typing import Union, Dict, List, Any
 import argparse
 import logging
 import pathlib
@@ -12,7 +8,6 @@ from requests.exceptions import SSLError
 
 from thoughtspot.models.metadata import MetadataObject
 from thoughtspot.util.datetime import to_datetime
-from thoughtspot.util.swagger import to_array
 from thoughtspot.util.ux import FrontendArgumentParser
 from thoughtspot.const import FMT_TSLOAD_DATETIME
 
@@ -42,131 +37,70 @@ def _to_friendly_name(metadata_type: str) -> str:
     return mapping.get(metadata_type, metadata_type)
 
 
-def _to_url(host: str, metadata: FLAT_API_RESPONSE) -> str:
-    """
-    Build a frontend URL for a given metadata object.
-
-    TODO: finalize and move to util.ux
-    """
-    raise NotImplementedError('need to smoke test <mapping>')
-    mapping = {
-        'QUESTION_ANSWER_BOOK': 'saved-answer',
-        'PINBOARD_ANSWER_BOOK': 'pinboard',
-        'LOGICAL_TABLE': 'data/tables',
-        'USER_DEFINED': 'data/tables',
-        'ONE_TO_ONE_LOGICAL': 'data/tables',
-        'AGGR_WORKSHEET': 'data/tables',
-        'INSIGHT': 'insight'
-    }
-
-    guid  = metadata['guid']
-    type_ = metadata['type']
-
-    try:
-        page = mapping[type_]
-    except KeyError:
-        return 'n/a'
-
-    return f'{host}/#/{page}/{guid}'
-
-
-def _get_metadata(api: 'ThoughtSpot', *, metadata_type: str) -> FLAT_API_RESPONSE:
+def _get_metadata(
+    api: 'ThoughtSpot',
+    *,
+    metadata_type: Union[str, List[str]]=None
+) -> FLAT_API_RESPONSE:
     """
     Return a flat data structure of metadata objects.
 
-    Data returns about each dependent:
+    Data returns about each object:
         - guid
         - type
         - name
-    """
-    r = api._metadata.list(type=metadata_type, batchsize=-1, showhidden=True)
-    objects = []
-
-    for metadata_object in r.json()['headers']:
-        objects.append({
-            'guid': metadata_object['id'],
-            'type': metadata_type,
-            'name': metadata_object['name']
-        })
-
-    return objects
-
-
-def _get_dependents(
-    api: 'ThoughtSpot',
-    *,
-    metadata_type: str='LOGICAL_TABLE'
-) -> FLAT_API_RESPONSE:
-    """
-    Return a flat data structure of dependents.
-
-    Data returned about each dependent:
-        - parent_guid
-        - parent_type
-        - parent_name
-        - parent_url
-        - guid
-        - type
-        - name
-        - url
         - author_name
         - author_display_name
         - created_at
         - modified_at
     """
-    parents = _get_metadata(api, metadata_type=metadata_type)
-    guids = map(lambda e: e['guid'], parents)
-    r = api._dependency.list_dependents(type=metadata_type, id=to_array(guids))
+    if metadata_type is None:
+        metadata_types = ...
+    elif isinstance(metadata_type, str):
+        metadata_types = [metadata_type]
 
-    dependencies = []
+    metadata = []
 
-    for parent_guid, dependents_info in r.json().items():
-        parent = next((p for p in parents if p['guid'] == parent_guid))
+    for metadata_type in metadata_types:
+        r = api._metadata.list(type=metadata_type, batchsize=-1, showhidden=False)
+        data = r.json().get('headers', [])
 
-        for dependent_type, dependents in dependents_info.items():
-            for dependent in dependents:
-                dependent['type'] = dependent.get('type', dependent_type)
+        for point in data:
+            metadata.append({
+                'guid': point['id'],
+                'type': _to_friendly_name(point['type']),
+                'name': point['name'],
+                'author_name': point['authorName'],
+                'author_display_name': point['authorDisplayName'],
+                'created_at': to_datetime(point['created'], unit='ms').strftime(FMT_TSLOAD_DATETIME),
+                'modified_at': to_datetime(point['modified'], unit='ms').strftime(FMT_TSLOAD_DATETIME)
+            })
 
-                dependencies.append({
-                    'parent_guid': parent['guid'],
-                    'parent_type': _to_friendly_name(parent['type']),
-                    'parent_name': parent['name'],
-                    'parent_url': _to_url(api.host, parent),
-                    'guid': dependent['id'],
-                    'type': _to_friendly_name(dependent['type']),
-                    'name': dependent['name'],
-                    'url': _to_url(api.host, dependent),
-                    'author_name': dependent['authorName'],
-                    'author_display_name': dependent['authorDisplayName'],
-                    'created_at': to_datetime(dependent['created'], unit='ms').strftime(FMT_TSLOAD_DATETIME),
-                    'modified_at': to_datetime(dependent['modified'], unit='ms').strftime(FMT_TSLOAD_DATETIME)
-                })
-
-    return dependencies
+    return metadata
 
 
 def app(api: 'ThoughtSpot', *, filename: str, metadata_type: str) -> None:
     """
     Main application logic.
 
-    This app will grab all the worksheet/view/table dependents from the
-    API and then save that data at <filename> in CSV format. This data
-    can be manually observed with a tool like Microsoft Excel, or
-    reimported back into ThoughtSpot and joined to the TS BI: Sever
-    table on the parent_guid.
+    This app will grab all the objects in a platform from the API and
+    then save that data at <filename> in CSV format. This data can be
+    manually observed with a tool like Microsoft Excel, or reimported
+    back into ThoughtSpot and joined to the TS BI: Sever table on the
+    guid.
     """
     try:
         with api:
-            dependencies = _get_dependents(api, metadata_type=metadata_type)
+            metadata = _get_metadata(api, metadata_type=metadata_type)
     except SSLError:
         msg = 'SSL certificate verify failed, did you mean to use flag --disable_ssl?'
         _log.error(msg)
         return
 
     with open(filename, mode='w', encoding='utf-8', newline='') as c:
-        writer = csv.DictWriter(c, dependencies[0].keys())
+        writer = csv.DictWriter(c, metadata[0].keys())
         writer.writeheader()
-        writer.writerows(dependencies)
+        writer.writerows(metadata)
 
 
 def parse_arguments() -> argparse.Namespace:
