25307025e0dde5df16c55b0c0433562576eb5abd
Nick Cooper <nicholas.cooper@thoughtspot.com>
2020-10-12 15:37:28 -0500

    minor docs changes

--- tools/shard recommender/insert_data.sh
+++ tools/shard recommender/insert_data.sh
@@ -1,34 +1,21 @@
-print_help () {
-  ECHO "insert_data.sh: periscope setup script"
-  ECHO "-t|--type REQUIRED"
-  ECHO "     type of periscope called (event, alert, table_info)"
-  ECHO ""
-}
-
 insert_data() {
     SOURCE_FILE=$1
     TARGET_TABLE=$2
-    LOG_FILE=$3
 
     tsload \
       --source_file ${SOURCE_FILE} \
-      --target_database TS_CUSTOM_PERISCOPE_SERVICE_DB \
+      --target_database cs_tools \
       --target_table ${TARGET_TABLE} \
       --has_header_row \
-      --field_separator \| \
+      --field_separator "," \
       --null_value "" \
       --boolean_representation True_False \
-      --date_time_format "%Y-%m-%d %H:%M:%S.%f" \
-    >> ${LOG_FILE} 2>&1
-
+      --date_time_format "%Y-%m-%dT%H:%M:%S" \
 }
 
 
-if [ -z "$*" ]; then
-  log_it "ERROR" "no arguments provided"
-  print_help
-  exit 1
-fi
-
-
-insert_data $*
+for file in $1; do
+    SOURCE_FILE=$(echo "${file}")
+    TARGET_TABLE=$(basename ${file} .csv)
+    insert_data SOURCE_FILE TARGET_TABLE
+done
