c82597da3032a8fc90be1dc0ed04acf4cad2d8b0
boonhapus <nicholas.cooper@thoughtspot.com>
2021-06-18 16:12:47 -0500

    🐛 larger timeout

--- cs_tools/api.py
+++ cs_tools/api.py
@@ -29,7 +29,7 @@ class ThoughtSpot:
 
         # set up our session
         # NOTE: base_url is a valid parameter for httpx.Client
-        self.http = httpx.Client(timeout=10.0, verify=not ts_config.thoughtspot.disable_ssl)
+        self.http = httpx.Client(timeout=180.0, verify=not ts_config.thoughtspot.disable_ssl)
         self.http.headers.update({'X-Requested-By': 'ThoughtSpot'})
 
         # set in __enter__()
