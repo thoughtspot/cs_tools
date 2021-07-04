3fb2005dd3df06435b96a1e37e4c4d854456f26f
boonhapus <nicholas.cooper@thoughtspot.com>
2021-07-04 17:03:07 -0500

    🔁 uvicorn watchdog-reload for dev purposes

--- cs_tools/tools/_cls-sharing/app.py
+++ cs_tools/tools/_cls-sharing/app.py
@@ -83,4 +83,10 @@ def run(
 ):
     """
     """
-    uvicorn.run('cs_tools.tools._cls-sharing.app:web_app', host='127.0.0.1', port=5000, log_level='info')
+    uvicorn.run(
+        'cs_tools.tools._cls-sharing.app:web_app',
+        host='127.0.0.1',
+        port=5000,
+        reload=True,
+        log_level='info'
+    )
