a2068fece86f87b18b7dfce9850a09500987ca40
boonhapus <nicholas.cooper@thoughtspot.com>
2021-08-22 17:20:48 -0500

    🔐 integrate security tools into a single app

--- cs_tools/tools/security-sharing/app.py
+++ cs_tools/tools/security-sharing/app.py
@@ -1,14 +1,18 @@
 from typing import Optional, List
 import logging
+import socket
 import enum
 
-from typer import Argument as A_, Option as O_
+from typer import Argument as A_, Option as O_  # noqa
+import uvicorn
 import typer
 
 from cs_tools.helpers.cli_ux import console, frontend, RichGroup, RichCommand
+from cs_tools.models.security import SharePermission
 from cs_tools.settings import TSConfig
 from cs_tools.api import ThoughtSpot
-from cs_tools.models.security import SharePermission
+
+from .web_app import _scoped
 
 
 log = logging.getLogger(__name__)
@@ -18,6 +22,21 @@ class PermissionType(str, enum.Enum):
     VIEW = 'view'
     EDIT = 'edit'
     REMOVE = 'remove'
+
+
+def _find_my_local_ip() -> str:
+    """
+    Gets the local ip, or loopback address if not found.
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.connect(('10.255.255.255', 1))  # does not need to be a valid addr
+
+        try:
+            ip = sock.getsockname()[0]
+        except IndexError:
+            ip = '127.0.0.1'
+
+    return ip
 
 
 def _get_group_id(api: ThoughtSpot, *, group_name: str) -> Optional[str]:
@@ -100,10 +119,44 @@ def _permission_param_to_permission(permission: str) -> SharePermission:
 
 app = typer.Typer(
     help="""
-    Share one or more tables from a database with a specified user group.
+    Scalably manage your table- and column-level security right in the browser.
+
+    Setting up Column Level Security (especially on larger tables) can be time-consuming
+    when done directly in the ThoughtSpot user interface. The web interface provided by
+    this tool will allow you to quickly understand the current security settings for a
+    given table across all columns, and as many groups as are in your platform. You may
+    then set the appropriate security settings for those group-table combinations.
     """,
     cls=RichGroup
 )
+
+
+@app.command(cls=RichCommand)
+@frontend
+def run(
+    webserver_port: int=O_(5000, help='port to host the webserver on'),
+    **frontend_kw
+):
+    """
+    Start the built-in webserver which runs the security management interface.
+    """
+    cfg = TSConfig.from_cli_args(**frontend_kw, interactive=True)
+    visit_ip = _find_my_local_ip()
+
+    with ThoughtSpot(cfg) as api:
+        _scoped['api'] = api
+
+        console.print(
+            'starting webserver...'
+            f'\nplease visit [green]http://{visit_ip}:5000/[/] in your browser'
+        )
+
+        uvicorn.run(
+            'cs_tools.tools._cls-sharing.web_app:web_app',
+            host='0.0.0.0',
+            port=webserver_port,
+            # log_config=None   # TODO log to file instead of console (less confusing for user)
+        )
 
 
 @app.command(cls=RichCommand)
