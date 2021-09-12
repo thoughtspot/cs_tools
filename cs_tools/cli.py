aacd44a79ef57edcd46630620ab35f63db76a1ac
boonhapus <nicholas.cooper@thoughtspot.com>
2021-09-11 21:09:00 -0500

    🪓 proper logging support

--- cs_tools/cli.py
+++ cs_tools/cli.py
@@ -42,7 +42,7 @@ app = typer.Typer(
     add_completion=False,
     context_settings={
         'help_option_names': ['--help', '-h'],
-        'max_content_width': 105,
+        'max_content_width': 125,
         'token_normalize_func': lambda x: x.lower()  # allow case-insensitive commands
     }
 )
