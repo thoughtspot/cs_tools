13c839beacfd08117018ea613e14f5a6c10a5e65
boonhapus <nicholas.cooper@thoughtspot.com>
2021-10-03 23:14:07 -0500

    🧪 set up metadata tests

--- tests/rest_api/test_metadata.py
+++ tests/rest_api/test_metadata.py
@@ -5,22 +5,32 @@ from tests.fixtures import thoughtspot
 
 
 PARAMETERS = {
-    'list_viz_headers': {
-        'id': 'bf2b2c6d-d29d-49f3-814c-00a3c40e77b0'
+    'metadata': {
+        'list_viz_headers': {
+            'id': 'bf2b2c6d-d29d-49f3-814c-00a3c40e77b0'
+        },
+        'list_object_headers': {
+            'type': 'LOGICAL_TABLE',
+            'subtypes': ['WORKSHEET', 'USER_DEFINED']
+        }
     },
-    'list_object_headers': {
-        'type': 'LOGICAL_TABLE',
-        'subtypes': ['WORKSHEET', 'USER_DEFINED']
+    '_metadata': {
+        'list': {},
+        'listas': {},
+        'detail': {},
+        'delete': {},
+        'list_columns': {}
     }
 }
 
 
-for endpoint, data in PARAMETERS.items():
+for family, tests in PARAMETERS.items():
+    for endpoint, data in tests.items():
 
-    @test('model adheres to contract: metadata.{endpoint}', tags=['unit'])
-    @using(ts=thoughtspot)
-    def _(ts, endpoint=endpoint):
-        data = PARAMETERS[endpoint]
-        meth = getattr(ts._rest_api.metadata, endpoint)
-        resp = meth(**data)
-        assert resp.status_code == httpx.codes.OK
+        @test('model adheres to contract: {family}.{endpoint}', tags=['unit'])
+        @using(ts=thoughtspot)
+        def _(ts, family=family, endpoint=endpoint, data=data):
+            apis = getattr(ts._rest_api, family)
+            meth = getattr(apis, endpoint)
+            resp = meth(**data)
+            assert resp.status_code == httpx.codes.OK
