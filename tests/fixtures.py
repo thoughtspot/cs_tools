b66e222a37142267cae9cf68529d23017971473a
boonhapus <nicholas.cooper@thoughtspot.com>
2021-10-09 10:57:40 -0500

    🧹 cleanup

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -8,8 +8,8 @@ from cs_tools.cli import _gather_tools, app as app_, tools_app, cfg_app, log_app
 
 @fixture(scope='global')
 def thoughtspot():
-    cfg = TSConfig.from_toml('tests/_test_config_6-3-1.toml')
-    # cfg = TSConfig.from_toml('tests/_test_config_cloud.toml')
+    # cfg = TSConfig.from_toml('tests/_test_config_6-3-1.toml')
+    cfg = TSConfig.from_toml('tests/_test_config_cloud.toml')
 
     with ThoughtSpot(config=cfg) as ts:
         yield ts
