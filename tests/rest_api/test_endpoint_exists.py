8ca21a720d32d9cef2c5615e6543765cf0839231
boonhapus <nicholas.cooper@thoughtspot.com>
2021-10-08 19:31:01 -0500

    🧪 add security and session tests

--- tests/rest_api/test_endpoint_exists.py
+++ tests/rest_api/test_endpoint_exists.py
@@ -6,40 +6,41 @@ from tests.fixtures import thoughtspot
 
 PARAMETERS = {
     'public': [
-        ('GET', 'metadata/listvizheaders'),
-        ('GET', 'metadata/listobjectheaders'),
-        ('GET', 'user/list'),
+        ( 'GET', 'metadata/listvizheaders'),
+        ( 'GET', 'metadata/listobjectheaders'),
+        ( 'GET', 'user/list'),
         ('POST', 'user/transfer/ownership')
     ],
     'private': [
-        ('GET', 'metadata/list'),
-        ('GET', 'metadata/listas'),
-        ('GET', 'metadata/detail/this-is-a-fake-guid'),
+        ( 'GET', 'metadata/list'),
+        ( 'GET', 'metadata/listas'),
+        ( 'GET', 'metadata/detail/this-is-a-fake-guid'),
         ('POST', 'metadata/delete'),
-        ('GET', 'metadata/listcolumns/this-is-a-fake-guid'),
-        ('GET', 'session/group/listuser/this-is-a-fake-guid'),
-        ('GET', 'session/group/listgroup/this-is-a-fake-guid'),
+        ( 'GET', 'metadata/listcolumns/this-is-a-fake-guid'),
+        ('POST', 'security/share'),
+        ('POST', 'security/definedpermission'),
+        ( 'GET', 'session/group/listuser/this-is-a-fake-guid'),
+        ( 'GET', 'session/group/listgroup/this-is-a-fake-guid'),
+        ( 'GET', 'session/info'),
     ],
     'dataservice': [
-        ('GET', 'tql/tokens/static'),
-        ('GET', 'tql/tokens/dynamic'),
+        ( 'GET', 'tql/tokens/static'),
+        ( 'GET', 'tql/tokens/dynamic'),
         ('POST', 'tql/query'),
         ('POST', 'tql/script'),
         ('POST', 'tsload/session'),
         ('POST', 'tsload/loads'),
         ('POST', 'tsload/loads/this-is-a-fake-cycle-id'),
         ('POST', 'tsload/loads/this-is-a-fake-cycle-id/commit'),
-        ('GET', 'tsload/loads/this-is-a-fake-cycle-id'),
+        ( 'GET', 'tsload/loads/this-is-a-fake-cycle-id'),
     ]
 }
 
 
 for privacy, endpoints in PARAMETERS.items():
     for method, endpoint in endpoints:
-        # TODO:
-        #   if this is a cloud environment, then we need to xfail on tsload cmds.
 
-        @test('endpoint exists: {method: <4} {privacy: >11} {endpoint}', tags=['unit', 'exists'])
+        @test('path exists: {method: <4} {privacy: >11} {endpoint}', tags=['unit', 'exists'])
         @using(ts=thoughtspot)
         def _(ts, method=method, endpoint=endpoint, privacy=privacy):
             # handle tsload being located at resource port 8442
@@ -54,7 +55,7 @@ for privacy, endpoints in PARAMETERS.items():
                     r = ts._rest_api.request(method, endpoint, privacy=privacy, timeout=3)
                     status = r.status_code
                     break
-                except httpx.ConnectTimeout:
+                except httpx.ConnectTimeout:  # akin to xfail, mostly for tsload on cloud
                     status = 504
                 except httpx.HTTPError as exc:
                     status = exc.response.status_code
