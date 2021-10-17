c1c2b3e2c3294d5285cf9e060a395fee12395499
boonhapus <nicholas.cooper@thoughtspot.com>
2021-10-17 16:03:55 -0500

    🕹 save state

--- cs_tools/tools/archiver/app.py
+++ cs_tools/tools/archiver/app.py
@@ -1,43 +1,78 @@
-# DEV NOTE:
-#
+import pathlib
+import enum
 
 from typer import Argument as A_, Option as O_  # noqa
 import typer
 
-from cs_tools.helpers.cli_ux import RichGroup
+from cs_tools.helpers.cli_ux import console, frontend, RichGroup, RichCommand
+from cs_tools.settings import TSConfig
+from cs_tools.thoughtspot import ThoughtSpot
+
+
+class ContentType(enum.Enum):
+    answer = 'answer'
+    pinboard = 'pinboard'
+    all = 'all'
 
 
 app = typer.Typer(
     help="""
-    One-liner describing the tool.
+    Archiver.
 
-    Further explanation explaining the tool's usage or purpose. This can
-    be as long as is necessary, but be mindful of much content you type
-    here as the full text will display in the console when the user
-    types...
-
-      cs_tools tools my-cool-app --help
-
-    If more ideas need to be conveyed, use separate paragraphs. Content
-    will be wrapped to the console spec (default: 125 characters) unless
-    you use a control character.
-
-    Many tools augment a ThoughtSpot service. If they do, a relevant
-    documentation link should be provided.
-
-    \b
-    For further information on <idea expressed in doc>, please refer to:
-      https://docs.thoughtspot.com/path/to/documenation-link.html
-
-    \f
-    DEV NOTE:
-
-      Two control characters are offered in order to help with
-      docstrings in typer App helptext and command helptext
-      (docstrings).
-
-      \b - Preserve Whitespace / formatting.
-      \f - EOF, don't include anything after this character in helptext.
+    Solution should help the customer identify objects which have not
+    been visited within a certain timeframe.
     """,
     cls=RichGroup
 )
+
+
+@app.command(cls=RichCommand)
+@frontend
+def fetch(
+    tag: str=O_(..., help='tag name to use for labeling objects to archive'),
+    content: ContentType=O_('all', help=''),
+    months: int=O_('all', help=''),
+    dry_run: bool=O_(False, '--dry-run', help=''),
+    **frontend_kw
+):
+    """
+    Identify objects which objects can be archived.
+    """
+    cfg = TSConfig.from_cli_args(**frontend_kw, interactive=True)
+
+    with ThoughtSpot(cfg) as ts:
+        ...
+
+
+@app.command(cls=RichCommand)
+@frontend
+def annul(
+    # tag: str=O_(),
+    # dry_run: bool=O_(),
+    **frontend_kw
+):
+    """
+    Unarchive objects.
+    """
+    cfg = TSConfig.from_cli_args(**frontend_kw, interactive=True)
+
+    with ThoughtSpot(cfg) as ts:
+        ...
+
+
+@app.command(cls=RichCommand)
+@frontend
+def delete(
+    # tag: str=O_(),
+    # months: int=O_(),
+    # export: pathlib.Path=O_(),
+    # dry_run: bool=O_(),
+    **frontend_kw
+):
+    """
+    Remove objects from the platform.
+    """
+    cfg = TSConfig.from_cli_args(**frontend_kw, interactive=True)
+
+    with ThoughtSpot(cfg) as ts:
+        ...
