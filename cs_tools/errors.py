b1b365fc8a911e59ca26008ea3dd1608806ffdfe
boonhapus <nicholas.cooper@thoughtspot.com>
2021-10-26 23:09:21 -0500

    🧹 cleanup

--- cs_tools/errors.py
+++ cs_tools/errors.py
@@ -11,28 +11,32 @@ class ContentDoesNotExist(CSToolsException):
     """
     Raised when ThoughtSpot can't find content by this name or guid.
     """
-    def __init__(self, type: str, name: str=None, guid: str=None):
+    def __init__(self, type: str, name: str = None, guid: str = None, reason: str = None):
         self.type = type
         self.guid = guid
         self.name = name
+        self.reason = reason
 
     @property
     def message(self) -> str:
         """
         Exception reason.
         """
+        if self.reason is not None:
+            return f'No '{self.type}' found for {self.reason}'
+
         if self.guid is not None:
-            return f"No {self.type} found with guid '{self.guid}'"
+            return f"No '{self.type}' found with guid '{self.guid}'"
 
         if self.name is not None:
-            return f"No {self.type} found with name '{self.guid}'"
+            return f"No '{self.type}' found with name '{self.guid}'"
 
 
 class AmbiguousContentError(CSToolsException):
     """
     Raised when ThoughtSpot can't determine an exact content match.
     """
-    def __init__(self, name: str, type: str=None):
+    def __init__(self, name: str, type: str = None):
         self.name = name
         self.type = type
 
