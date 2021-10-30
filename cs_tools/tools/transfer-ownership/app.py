f76ef0dad34d67b5528f9d5ed56fd6e02103c9d4
boonhapus <nicholas.cooper@thoughtspot.com>
2021-10-30 16:37:38 -0500

    🚇 add ability to transfer only certain objects

--- cs_tools/tools/transfer-ownership/app.py
+++ cs_tools/tools/transfer-ownership/app.py
@@ -1,11 +1,12 @@
+from typing import List
 import json
 
 from typer import Argument as A_, Option as O_
 import typer
 
-from cs_tools.helpers.cli_ux import console, frontend, RichGroup, RichCommand
-from cs_tools.settings import TSConfig
+from cs_tools.helpers.cli_ux import _csv, console, frontend, RichGroup, RichCommand
 from cs_tools.thoughtspot import ThoughtSpot
+from cs_tools.settings import TSConfig
 
 
 app = typer.Typer(
@@ -19,8 +20,10 @@ app = typer.Typer(
 @app.command(cls=RichCommand)
 @frontend
 def transfer(
-    from_: str=O_(..., '--from', help='username of the current content owner'),
-    to_: str=O_(..., '--to', help='username to transfer content to'),
+    from_: str=A_(..., metavar='FROM', help='username of the current content owner'),
+    to_: str=A_(..., metavar='TO', help='username to transfer content to'),
+    tag: List[str]=O_(None, callback=_csv, help='if specified, only move content marked with these tags'),
+    guids: List[str]=O_(None, callback=_csv, help='if specified, only move specific objects'),
     **frontend_kw
 ):
     """
@@ -28,16 +31,31 @@ def transfer(
     """
     cfg = TSConfig.from_cli_args(**frontend_kw, interactive=True)
 
+    console.print(f'\nTransferring all objects from "{from_}" to "{to_}"')
+    ids = set()
+
     with ThoughtSpot(cfg) as ts:
-        console.print(f'\nTransferring all objects from "{from_}" to "{to_}"')
+
+        if tag is not None or guids is not None:
+            r = ts.api._metadata.list(type='USER', pattern=f'%{from_}%')
+            r = ts.api._metadata.listas(type='USER', principalid=r.json()['headers'][0]['id'])
+
+            if tag is not None:
+                ids.update([_ for _ in r.json()['headers'] if _['tag'] in tag])
+
+            if guids is not None:
+                ids.update([_ for _ in r.json()['headers'] if _['id'] in guids])
 
         with console.status('[bold green]running query[/]'):
-            r = ts.api.user.transfer_ownership(from_, to_)
-
-        try:
-            r.raise_for_status()
-            console.print('[green]Success![/]')
-        except Exception:
-            json_msg = r.json()['debug']
-            msg = json.loads(json_msg)  # uhm, lol?
-            console.print(f'[red]Failed. {msg[-1]}[/]')
+            try:
+                r = ts.api.user.transfer_ownership(
+                        fromUserName=from_,
+                        toUserName=to_,
+                        objectsID=ids
+                    )
+            except Exception:
+                json_msg = r.json()['debug']
+                msg = json.loads(json_msg)  # uhm, lol?
+                console.print(f'[red]Failed. {msg[-1]}[/]')
+            else:
+                console.print('[green]Success![/]')
