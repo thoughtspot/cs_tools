5ffcdc40a037820d573ef02be59a15296353dffa
boonhapus <nicholas.cooper@thoughtspot.com>
2022-04-10 14:07:36 -0500

    ✔ add test for working environment in vendor script

--- scripts/_vendor-cs_tools.py
+++ scripts/_vendor-cs_tools.py
@@ -7,10 +7,35 @@ difficult to manage by hand across 25+ configurations.
 
 Additionally, when new python versions, dependency requirements, or unix OS versions are
 released, we need augment the distribution. This scaffolding helps do just that.
+
+Files will be written under the project root /dist subdirectory.
+
+    cs_tools/
+    ├─ ...
+    │
+    ├─ dist/
+    │  ├─ windows/
+    │  │  ├─ dependency-1.whl
+    │  │  ├─ ...
+    │  │  └─ dependency-N.whl
+    │  ├─ linux/
+    │  │  ├─ ...
+    │  │  └─ ...
+    │  ├─ mac/
+    │  │  ├─ ...
+    │  │  └─ ...
+    │  │
+    │  └─ req/
+    │     ├─ offline-install.txt
+    │     └─ requirements.txt
+    │
+    └─ ...
+
 """
 from zipfile import ZipFile, ZIP_DEFLATED
 import subprocess as sp
 import itertools as it
+import platform
 import logging
 import pathlib
 import sys
@@ -18,6 +43,8 @@ import sys
 
 log = logging.getLogger(__name__)
 HERE = pathlib.Path(__file__).parent
+DIST = HERE.parent / 'dist'
+
 SUPPORTED_PYTHON_VERSIONS = ('3.6.8', '3.7', '3.8', '3.9', '3.10')
 SUPPORTED_ARCHITECTURES = {
     'windows': (
@@ -47,6 +74,19 @@ def in_virtual_env() -> bool:
     return (base_prefix or real_prefix) != sys.prefix
 
 
+def get_system_name() -> str:
+    """
+    Translate `this` system into a friendlier name.
+
+    Returns
+    -------
+    system: str
+        one of.. windows, linux, mac
+    """
+    translate = {'Windows': 'windows', 'Linux': 'linux', 'Darwin': 'mac'}
+    return translate[platform.uname().system]
+
+
 def pip(*args) -> str:
     """
     Use pip, programmatically.
@@ -72,19 +112,27 @@ if __name__ == '__main__':
     logging.basicConfig(format='[%(asctime)s] %(message)s', datefmt='%H:%M:%S', level='INFO')
 
     if not in_virtual_env():
-        log.warning('not running in virtual environment.. activate it first!')
+        log.warning('not running in virtual environment.. please activate it first!')
         raise SystemExit(-1)
 
-    # it's a hack to get the current version, assuming you won't attempt to run this
-    # script unless CS Tools is actually installed :~)
-    __version__ = __import__('cs_tools._version').__version__
+    try:
+        import cs_tools
+    except ModuleNotFoundError:
+        log.warning(
+            f'import of cs_tools failed, you might have a broken local install!\n\n'
+            f'try running..\n\n\tpip install -r {HERE.parent}/dev-requirements.txt\n\n'
+            f'and then re-run this script\n\n\t{" ".join(sys.argv)}'
+        )
+        raise SystemExit(-1)
 
-    # uninstall all dependencies to reset the environment
-    log.info('uninstalling all packages')
+    __version__ = cs_tools._version.__version__
+    # uninstall all dependencies and reset the environment
+    log.info('uninstalling all packages and deleting pip\'s cache')
     now_frozen = pip('freeze').strip().split('\r\n')
     reqs = [_ for _ in now_frozen if 'cs_tools' not in _ if _]
     reqs.append('cs_tools')
     pip('uninstall', '-y', *reqs)
+    pip('cache', 'purge')
 
     # install from requirements.txt
     log.info('installing packages from requirements.txt')
@@ -92,16 +140,14 @@ if __name__ == '__main__':
     pip('install', '-r', reqs)
 
     # download all packages..
-    dist = HERE.parent / 'dist'
-
-    for platform, architectures in SUPPORTED_ARCHITECTURES.items():
-        log.info(f'downloading packages for {platform}..')
+    for platform_name, architectures in SUPPORTED_ARCHITECTURES.items():
+        log.info(f'downloading packages for {platform_name}..')
         for py_version in SUPPORTED_PYTHON_VERSIONS:
             log.info(f'\t on  py{py_version: <5}')
             pip(
                 'download',
                 '-r', reqs,
-                '--dest', dist / platform,
+                '--dest', DIST / platform_name,
                 '--implementation', 'cp',
                 *it.chain.from_iterable(('--platform', f'{a}') for a in architectures),
                 '--python-version', py_version,
@@ -110,7 +156,7 @@ if __name__ == '__main__':
 
         # vendor cs_tools itself
         log.info(f'\t vendoring cs_tools=={__version__}\n')
-        with ZipFile(dist / platform / f'cs_tools-{__version__}.zip', mode='w', compression=ZIP_DEFLATED) as z:
+        with ZipFile(DIST / platform_name / f'cs_tools-{__version__}.zip', mode='w', compression=ZIP_DEFLATED) as z:
             dir_ = HERE.parent.expanduser().resolve(strict=True)
 
             for stem in (
@@ -126,7 +172,31 @@ if __name__ == '__main__':
 
                 z.write(file, file.relative_to(dir_))
 
+    # install environment using the local option..
+    log.info('installing in virtual environment using offline-install.txt')
+    pip(
+        'install',
+        '-r',
+        f'{DIST}/reqs/offline-install.txt',
+        f'--find-links={DIST}/{get_system_name()}/',
+        '--no-cache-dir',
+        '--no-index'
+    )
+
+    log.info('checking if cs_tools meets the current version')
+    r = sp.check_output(['cs_tools', '--version'])
+    a = r.decode().strip()
+    t = f'cs_tools ({__version__})'
+    
+    try:
+        assert a == t
+    except AssertionError as e:
+        log.info(f'\t failed! ❌\n\t   [found: {a}, expecting: {t}]')
+        raise
+    else:
+        log.info('\tsuccess! ✅')
+
     # re-install from dev-requirements.txt
-    log.info('installing packages from dev-requirements.txt')
+    log.info('resetting environment from dev-requirements.txt')
     reqs = HERE.parent / 'dev-requirements.txt'
     pip('install', '-r', reqs)
