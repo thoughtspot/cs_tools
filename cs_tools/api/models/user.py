f720ee0ddf83c7f9772391cb37041e6dcc0ab15c
boonhapus <nicholas.cooper@thoughtspot.com>
2022-04-17 19:37:12 -0500

    ✔ add user/sync

--- cs_tools/api/models/user.py
+++ cs_tools/api/models/user.py
@@ -1,10 +1,16 @@
-from typing import List
+from typing import Any, Dict, List
+import datetime as dt
+import logging
+import json
 
 from pydantic import validate_arguments
 import httpx
 
 from cs_tools.api.util import stringified_array
 from cs_tools.data.enums import GUID
+
+
+log = logging.getLogger(__name__)
 
 
 class User:
@@ -41,6 +47,40 @@ class User:
                     # technically not available until ts7.sep.cl-109 or greater, but
                     # query parameters don't usually cause 4xx or 5xx errors
                     'objectid': stringified_array(objectid or ())
+                }
+            )
+        return r
+
+    @validate_arguments
+    def sync(
+        self,
+        principals: List[Dict[str, Any]],
+        applyChanges: bool = False,
+        removeDeleted: bool = True,
+        password: str = None
+    ) -> httpx.Response:
+        """
+        Api to synchronize principal from external system with ThoughtSpot system
+        """
+        dir_ = self.rest_api._config.temp_dir / 'user_syncs'
+        dir_.mkdir(parents=True, exist_ok=True)
+
+        now = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        fp = dir_ / f'principals-{now}.json'
+
+        with fp.open('w') as f:
+            log.debug(f'writing principals (before sync) to {fp}')
+            json.dump(principals, f, indent=4)
+
+        r = self.rest_api.request(
+                'POST',
+                'user/sync',
+                privacy='public',
+                files={'principals': ('principals.json', fp.open('rb'), 'application/json')},
+                data={
+                    'applyChanges': applyChanges,
+                    'removeDeleted': removeDeleted,
+                    'password': password
                 }
             )
         return r
