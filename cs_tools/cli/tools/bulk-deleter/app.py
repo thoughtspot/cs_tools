5ba0332acd66840ae22e19ef892ae0b74ddcd26f
boonhapus <nicholas.cooper@thoughtspot.com>
2022-04-21 16:12:01 -0500

    🐃 migrate bulk-deleter to use syncer

--- cs_tools/cli/tools/bulk-deleter/app.py
+++ cs_tools/cli/tools/bulk-deleter/app.py
@@ -1,17 +1,13 @@
-from typing import Dict
 import logging
 import pathlib
-import shutil
 import enum
-import csv
 
 from typer import Argument as A_, Option as O_  # noqa
-from openpyxl import load_workbook
 import typer
 
 from cs_tools.cli.dependency import depends
 from cs_tools.cli.options import CONFIG_OPT, VERBOSE_OPT, TEMP_DIR_OPT
-from cs_tools.cli.ux import CSToolsGroup, CSToolsCommand, console
+from cs_tools.cli.ux import console, CSToolsGroup, CSToolsCommand, SyncerProtocolType
 from cs_tools.cli.tools.common import setup_thoughtspot
 from cs_tools.util import chunks
 
@@ -48,65 +44,11 @@ class ReversibleSystemType(str, enum.Enum):
         return getattr(cls, value.replace(' ', '_')).value
 
 
-def _from_csv(fp: pathlib.Path) -> Dict[str, str]:
-    """
-    Read data from a CSV.
-    """
-    with fp.open() as f:
-        reader = csv.DictReader(f)
-        data = [row for row in reader]
-
-    return data
-
-
-def _from_excel(fp: pathlib.Path) -> Dict[str, str]:
-    """
-    Read data from an Excel file.
-
-    This will read data only from the first tab of the workbook.
-    """
-    wb = load_workbook(filename=fp.as_posix())
-    rows = wb.active.rows
-    headers = [c.value for c in next(rows)]
-    return [dict(zip(headers, [column.value for column in row])) for row in rows]
-
-
 app = typer.Typer(
-    help="""
-    Bulk delete metadata objects from your ThoughtSpot platform.
-
-    [b][yellow]USE AT YOUR OWN RISK![/b] This tool uses private API calls which
-    could change on any version update and break the tool.[/]
-    """,
+    help="""Bulk delete metadata objects from your ThoughtSpot platform.""",
     cls=CSToolsGroup,
     options_metavar='[--version, --help]',
 )
-
-
-@app.command(cls=CSToolsCommand)
-def generate_file(
-    export: pathlib.Path = O_(
-        ...,
-        help='filepath to save generated file to',
-        metavar='FILE.csv',
-        dir_okay=False,
-        resolve_path=True,
-        prompt=True
-    ),
-    # maintained for backwards compatability
-    backwards_compat: pathlib.Path = O_(None, '--save_path', help='backwards-compat if specified, directory to save data to', hidden=True)
-):
-    """
-    Generates example file in Excel or CSV format.
-    """
-    if export.suffix == '.xlsx':
-        shutil.copy(HERE / 'static' / 'template.xlsx', export)
-    elif export.suffix == '.csv':
-        shutil.copy(HERE / 'static' / 'template.csv', export)
-    else:
-        log.error('appropriate file not supplied, must be either Excel or CSV')
-        console.print(f'[red]must provide an Excel (.xlsx) or CSV (.csv) file, got {export}[/]')
-        return
 
 
 @app.command(cls=CSToolsCommand)
@@ -140,24 +82,26 @@ def single(
     options=[CONFIG_OPT, VERBOSE_OPT, TEMP_DIR_OPT],
     enter_exit=True
 )
-def from_file(
+def from_tabular(
     ctx: typer.Context,
-    file: pathlib.Path = A_(
-        ...,
-        help='path to a file with columns "type" and "guid"',
-        metavar='FILE.csv',
-        dir_okay=False,
-        resolve_path=True
+    syncer: str = O_(
+        None,
+        help='protocol and path for options to pass to the syncer',
+        metavar='protocol://DEFINITION.toml',
+        callback=lambda ctx, to: SyncerProtocolType().convert(to, ctx=ctx)
+    ),
+    deletion: str = O_(
+        None,
+        help='if using --syncer, directive to find user deletion at'
     ),
     batchsize: int = O_(1, help='maximum amount of objects to delete simultaneously')
 ):
     """
     Remove many objects from ThoughtSpot.
 
-    Accepts an Excel (.xlsx) file or CSV (.csv) file.
-
     \b
-    CSV/XLSX file format should look like..
+    If you are deleting from an external data source, your data must follow the
+    tabular format below.
 
     \b
         +----------------+-------+
@@ -169,16 +113,13 @@ def from_file(
         | saved answer   | guid3 |
         +----------------+-------+
     """
-    ts = ctx.obj.thoughtspot
+    if syncer is not None:
+        if deletion is None:
+            console.print('[red]you must provide a syncer directive to --deletion')
+            raise typer.Exit(-1)
 
-    if file.suffix == '.xlsx':
-        data = _from_excel(file)
-    elif file.suffix == '.csv':
-        data = _from_csv(file)
-    else:
-        log.error('appropriate file type not supplied, must be either Excel or CSV')
-        console.print(f'[red]must provide an Excel (.xlsx) or CSV (.csv) file, got {file}[/]')
-        return
+    ts = ctx.obj.thoughtspot
+    data = syncer.load(deletion)
 
     #
     # Delete Pinboards
