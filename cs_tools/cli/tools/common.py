55706b33fd17cc04c26a80e26847168cc717c1ca
boonhapus <nicholas.cooper@thoughtspot.com>
2022-04-24 16:49:23 -0500

    💀 remove dead code

--- cs_tools/cli/tools/common.py
+++ cs_tools/cli/tools/common.py
@@ -34,27 +34,3 @@ def setup_thoughtspot(
         ctx.obj.thoughtspot = ThoughtSpot(cfg)
 
     return ctx.obj.thoughtspot
-
-
-def to_csv(
-    data: List[Dict[str, Any]],
-    fp: pathlib.Path,
-    *,
-    mode: str = 'w',
-    sep: str = '|',
-    header: bool = False
-):
-    """
-    Write data to CSV.
-
-    Data must be in record format.. [{column -> value}, ..., {column -> value}]
-    """
-    header = header or not fp.exists()
-
-    with fp.open(mode=mode, encoding='utf-8', newline='') as c:
-        writer = csv.DictWriter(c, data[0].keys(), delimiter=sep)
-
-        if header:
-            writer.writeheader()
-
-        writer.writerows(data)
