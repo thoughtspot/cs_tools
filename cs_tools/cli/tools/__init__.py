2c6b6b6c4a0f78bd140a9b7c24eb00d4be5a353d
boonhapus <nicholas.cooper@thoughtspot.com>
2022-05-21 20:20:09 -0500

    🧠 refactor hidden options

--- cs_tools/cli/tools/__init__.py
+++ cs_tools/cli/tools/__init__.py
@@ -43,6 +43,25 @@ app = typer.Typer(
     clients who have a well-adopted platform.
     """,
     subcommand_metavar='<tool>',
-    callback=_show_hidden_tool_options,
     invoke_without_command=True
 )
+
+
+@app.callback()
+def _hidden_options(
+    ctx: typer.Context,
+    # Both options are completely hidden.
+    private: bool = O_(False, hidden=True),
+    beta: bool = O_(False, hidden=True)
+):
+    for tool_name, click_group in ctx.command.commands.items():
+        cs_tool = ctx.obj.tools[tool_name]
+
+        if private and cs_tool.privacy == 'private':
+            click_group.hidden = False
+
+        if beta and cs_tool.privacy == 'beta':
+            click_group.hidden = False
+
+    console.print(ctx.get_help())
+    raise typer.Exit(code=0)
