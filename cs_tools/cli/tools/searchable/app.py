48056c2bce6c47318c03904740035f6a4b46e132
boonhapus <nicholas.cooper@thoughtspot.com>
2022-06-09 12:16:20 -0500

    🐛 fix date format on bi-server

--- cs_tools/cli/tools/searchable/app.py
+++ cs_tools/cli/tools/searchable/app.py
@@ -150,14 +150,15 @@ def bi_server(
         - response size         - latency (us)          - database latency (us)
         - impressions
     """
+    SEARCH_DATA_DATE_FMT = "%m/%d/%Y"
     SEARCH_TOKENS = (
         "[incident id] [timestamp].'detailed' [url] [http response code] "
         "[browser type] [browser version] [client type] [client id] [answer book guid] "
         "[viz id] [user id] [user action] [query text] [response size] [latency (us)] "
         "[database latency (us)] [impressions]"
         + ("" if compact else " [user action] != {null} 'invalid'")
-        + ("" if from_date is None else f" [timestamp] >= '{from_date.date()}'")
-        + ("" if to_date is None else f" [timestamp] <= '{to_date.date()}'")
+        + ("" if from_date is None else f" [timestamp] >= '{from_date.strftime(SEARCH_DATA_DATE_FMT)}'")
+        + ("" if to_date is None else f" [timestamp] <= '{to_date.strftime(SEARCH_DATA_DATE_FMT)}'")
         + ("" if include_today else " [timestamp] != 'today'")
     )
 
