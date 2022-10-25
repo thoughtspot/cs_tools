0a92a453a15859c32f53aec1d7fa0fe106b386bd
boonhapus <nicholas.cooper@thoughtspot.com>
2022-10-25 16:30:52 -0500

    one version to rule them all

--- cs_tools/_version.py
+++ cs_tools/_version.py
@@ -1,1 +1,13 @@
-__version__ = '1.3.5'
+def _get_version() -> str:
+    """
+    Retrieve the version string.
+    """
+    import pathlib
+    import toml
+
+    package_dir = pathlib.Path(__file__).parent.parent
+    pyproject_toml = toml.load(package_dir / "pyproject.toml")
+    return pyproject_toml["tool"]["poetry"]["version"]
+
+
+__version__ = _get_version()
