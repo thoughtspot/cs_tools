f46c3359368d539d8d956068b47dfb7100830bc9
boonhapus <nicholas.cooper@thoughtspot.com>
2022-10-29 22:34:40 -0500

    implement a working syncer dependency injector

---
+++ cs_tools/cli/dependencies/syncer.py
@@ -0,0 +1,147 @@
+from typing import Any, Dict, List, Tuple
+from dataclasses import dataclass
+import logging
+import pathlib
+import sys
+
+from typer.core import TyperOption
+import pydantic
+import click
+import toml
+
+from cs_tools.cli.dependencies.base import Dependency
+from cs_tools.errors import CSToolsError, SyncerError
+from cs_tools.const import PACKAGE_DIR
+from cs_tools.sync import register
+from cs_tools.data import models
+
+log = logging.getLogger(__name__)
+
+
+@dataclass
+class DSyncer(Dependency):
+    protocol: str
+    definition_fp: pathlib.Path
+
+    def __enter__(self):
+        ctx = click.get_current_context()
+        proto = self.protocol
+        definition = self.definition_fp
+
+        if definition in ('default', ''):
+            ts_config = ctx.obj.thoughtspot.config
+
+            try:
+                definition = ts_config.syncer[proto]
+            except (TypeError, KeyError):
+                raise SyncerError(
+                    proto=proto,
+                    cfg=ts_config.name,
+                    reason="No default definition has been set for this cluster config.",
+                    mitigation=(
+                        "Pass the full path to [primary]{proto}://[/] or set a default "
+                        "with [primary]cs_tools config modify --config {cfg} --syncer "
+                        "{proto}://[blue]path/to/my/default.toml"
+                    )
+                )
+
+        try:
+            cfg = toml.load(definition)
+        except (IsADirectoryError, FileNotFoundError):
+            raise SyncerError(
+                proto=proto,
+                definition=definition,
+                reason="No {proto} definition found at [blue]{definition}",
+                mitigation="You must specify a valid path to a .toml definition file."
+            )
+        except UnicodeDecodeError:
+            raise SyncerError(
+                proto=proto,
+                definition=definition,
+                reason="Couldn't read the Syncer definition at [blue]{definition}[/]",
+                mitigation=(
+                    "If you're on Windows, you must escape the backslashes in your filepaths, or flip them the other "
+                    "way around.\n"
+                    "\n  :x: [red]" + r"C:\path\to\my\definition.toml" + "[/]"
+                    "\n  :white_heavy_check_mark: [green]" + r"C:\\path\\to\\my\\definition.toml" + "[/]"
+                    "\n  :white_heavy_check_mark: [green]" + r"C:/path/to/my/definition.toml" + "[/]"
+                )
+            )
+        except toml.TomlDecodeError:
+            raise SyncerError(
+                proto=proto,
+                definition=definition,
+                reason="Your definition file [blue]{definition}[/] is not correct.",
+                mitigation=(
+                    "Visit the link below to see a full example."
+                    "\n[blue]https://thoughtspot.github.io/cs_tools/syncer/{proto}/#full-definition-example"
+                )
+            )
+
+        if 'manifest' not in cfg:
+            cfg['manifest'] = PACKAGE_DIR / 'sync' / proto / 'MANIFEST.json'
+
+        log.info(f'registering syncer: {proto}')
+        Syncer = register.load_syncer(protocol=proto, manifest_path=cfg.pop('manifest'))
+
+        try:
+            # sanitize input by accepting aliases
+            if hasattr(Syncer, '__pydantic_model__'):
+                cfg['configuration'] = Syncer.__pydantic_model__.parse_obj(cfg['configuration']).dict()
+
+            self._syncer = syncer = Syncer(**cfg['configuration'])
+        except KeyError:
+            raise SyncerError(
+                proto=proto,
+                definition=definition,
+                reason="[blue]{definition}[/] is missing a top level marker.",
+                mitigation=(
+                    "The first line of your definition file should be.."
+                    "\n\n[white]\[configuration]"
+                )
+            )
+        except pydantic.ValidationError as e:
+            raise SyncerError(
+                proto=proto,
+                definition=definition,
+                errors='\n  '.join([f"[blue]{_['loc'][0]}[/]: {_['msg']}" for _ in e.errors()]),
+                reason="[blue]{definition}[/] has incorrect parameters.\n\n  {errors}",
+                mitigation=(
+                    "Visit the link below to see a full example."
+                    "\n[blue]https://thoughtspot.github.io/cs_tools/syncer/{proto}/#full-definition-example"
+                )
+            )
+
+        # don't actually make lasting changes, just ensure it initializes
+        # if validate_only:
+        #     return value
+
+        is_database_check = getattr(syncer, '__is_database__', False)
+        is_tools_cmd = 'tools' in sys.argv[1:]
+
+        if is_database_check or not is_tools_cmd:
+            if getattr(syncer, 'metadata', None) is not None:
+                metadata = syncer.metadata
+                [t.to_metadata(metadata) for t in models.SQLModel.metadata.sorted_tables]
+            else:
+                metadata = models.SQLModel.metadata
+
+            metadata.create_all(syncer.cnxn)
+
+            # DEV NOTE: conditionally expose ability to grab views
+            if syncer.name != 'falcon':
+                metadata.reflect(syncer.cnxn, views=True)
+ 
+    def __exit__(self, *e):
+        return
+
+    # proxy calls to the underlying syncer
+
+    def dump(self, identifier: str, *, data):
+        return self._syncer.dump(identifier, data=data)
+
+    def load(self, identifier: str) -> None:
+        return self._syncer.load(identifier)
+
+    def __repr__(self) -> str:
+        return self._syncer.__repr__()
--- cs_tools/cli/types.py
+++ cs_tools/cli/types.py
@@ -9,11 +9,8 @@ import pydantic
 import click
 import toml
 
-from cs_tools.errors import CSToolsError, SyncerError
+from cs_tools.cli.dependencies.syncer import DSyncer
 from cs_tools.sync.protocol import SyncerProtocol
-from cs_tools.const import PACKAGE_DIR
-from cs_tools.sync import register
-from cs_tools.data import models
 
 
 log = logging.getLogger(__name__)
@@ -87,112 +84,6 @@ class SyncerProtocolType(click.ParamType):
             return value
 
         proto, definition = value.split('://')
-
-        print(ctx.command.callback.dependencies)
-        raise SystemExit(-1)
-
-        if definition in ('default', ''):
-            ts_config = ctx.obj.thoughtspot.config
-
-            try:
-                definition = ts_config.syncer[proto]
-            except (TypeError, KeyError):
-                raise SyncerError(
-                    proto=proto,
-                    cfg=ts_config.name,
-                    reason="No default definition has been set for this cluster config.",
-                    mitigation=(
-                        "Pass the full path to [primary]{proto}://[/] or set a default "
-                        "with [primary]cs_tools config modify --config {cfg} --syncer "
-                        "{proto}://[blue]path/to/my/default.toml"
-                    )
-                )
-
-        try:
-            cfg = toml.load(definition)
-        except (IsADirectoryError, FileNotFoundError):
-            raise SyncerError(
-                proto=proto,
-                definition=definition,
-                reason="No {proto} definition found at [blue]{definition}",
-                mitigation="You must specify a valid path to a .toml definition file."
-            )
-        except UnicodeDecodeError:
-            raise SyncerError(
-                proto=proto,
-                definition=definition,
-                reason="Couldn't read the Syncer definition at [blue]{definition}[/]",
-                mitigation=(
-                    "If you're on Windows, you must escape the backslashes in your filepaths, or flip them the other "
-                    "way around.\n"
-                    "\n  :x: [red]" + r"C:\path\to\my\definition.toml" + "[/]"
-                    "\n  :white_heavy_check_mark: [green]" + r"C:\\path\\to\\my\\definition.toml" + "[/]"
-                    "\n  :white_heavy_check_mark: [green]" + r"C:/path/to/my/definition.toml" + "[/]"
-                )
-            )
-        except toml.TomlDecodeError:
-            raise SyncerError(
-                proto=proto,
-                definition=definition,
-                reason="Your definition file [blue]{definition}[/] is not correct.",
-                mitigation=(
-                    "Visit the link below to see a full example."
-                    "\n[blue]https://thoughtspot.github.io/cs_tools/syncer/{proto}/#full-definition-example"
-                )
-            )
-
-        if 'manifest' not in cfg:
-            cfg['manifest'] = PACKAGE_DIR / 'sync' / proto / 'MANIFEST.json'
-
-        log.info(f'registering syncer: {proto}')
-        Syncer = register.load_syncer(protocol=proto, manifest_path=cfg.pop('manifest'))
-
-        try:
-            # sanitize input by accepting aliases
-            if hasattr(Syncer, '__pydantic_model__'):
-                cfg['configuration'] = Syncer.__pydantic_model__.parse_obj(cfg['configuration']).dict()
-
-            syncer = Syncer(**cfg['configuration'])
-        except KeyError:
-            raise SyncerError(
-                proto=proto,
-                definition=definition,
-                reason="[blue]{definition}[/] is missing a top level marker.",
-                mitigation=(
-                    "The first line of your definition file should be.."
-                    "\n\n[white]\[configuration]"
-                )
-            )
-        except pydantic.ValidationError as e:
-            raise SyncerError(
-                proto=proto,
-                definition=definition,
-                errors='\n  '.join([f"[blue]{_['loc'][0]}[/]: {_['msg']}" for _ in e.errors()]),
-                reason="[blue]{definition}[/] has incorrect parameters.\n\n  {errors}",
-                mitigation=(
-                    "Visit the link below to see a full example."
-                    "\n[blue]https://thoughtspot.github.io/cs_tools/syncer/{proto}/#full-definition-example"
-                )
-            )
-
-        # don't actually make lasting changes, just ensure it initializes
-        if validate_only:
-            return value
-
-        is_database_check = getattr(syncer, '__is_database__', False)
-        is_tools_cmd = 'tools' in sys.argv[1:]
-
-        if is_database_check or not is_tools_cmd:
-            if getattr(syncer, 'metadata', None) is not None:
-                metadata = syncer.metadata
-                [t.to_metadata(metadata) for t in models.SQLModel.metadata.sorted_tables]
-            else:
-                metadata = models.SQLModel.metadata
-
-            metadata.create_all(syncer.cnxn)
-
-            # DEV NOTE: conditionally expose ability to grab views
-            if syncer.name != 'falcon':
-                metadata.reflect(syncer.cnxn, views=True)
-
-        return syncer
+        syncer_dependency = DSyncer(protocol=proto, definition_fp=definition, parameters=[])
+        ctx.command.callback.dependencies.append(syncer_dependency)
+        return syncer_dependency
