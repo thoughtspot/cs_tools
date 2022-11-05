d19d3d5cf8a8969f6513aaf66ef821e98dae74cf
boonhapus <nicholas.cooper@thoughtspot.com>
2022-11-05 16:12:23 -0500

    enforce LOGICAL_TABLE data types

--- cs_tools/api/middlewares/search.py
+++ cs_tools/api/middlewares/search.py
@@ -1,20 +1,23 @@
 from typing import Any, Dict, List
+import datetime as dt
 import logging
+import json
 
 from pydantic import validate_arguments
+import httpx
 
-from cs_tools.errors import AmbiguousContentError, ContentDoesNotExist
+from cs_tools.errors import CSToolsError, AmbiguousContentError, ContentDoesNotExist
 from cs_tools.api import util
 
 
 log = logging.getLogger(__name__)
+DB_ROW = Dict[str, Any]
 
 
-def _clean_datetime(row: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+def _fix_for_scal_101507(row: List[Dict[str, Any]]) -> List[DB_ROW]:
     # BUG: SCAL-101507
     #
-    # Prior to ts7.nov.cl-1xx / 7.2.1 release, DATE_TIME formatted columns would return
-    # data in the format below. This fixes that.
+    # DATE_TIME formatted columns return data in the format below. This fixes that.
     #
     # "data": [ {"Timestamp": {"v":{"s":1625759921}} ]
     #
@@ -26,6 +29,54 @@ def _clean_datetime(row: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
                 log.warning(f'unexpected value in search-data response: {value}')
 
     return row
+
+
+def _to_records(columns: List[str], rows: List[DB_ROW]) -> List[DB_ROW]:
+    return [dict(zip(columns, _fix_for_scal_101507(row))) for row in rows]
+
+
+def _cast(data: List[DB_ROW], types: Dict[str, str]) -> List[DB_ROW]:
+    """
+    Cast data coming back from Search API to their intended column types.
+    """
+    TS_TO_PY_TYPES = {
+        "VARCHAR": str,
+        "DOUBLE": float,
+        "FLOAT": float,
+        "BOOL": bool,
+        "INT32": int,
+        "INT64": int,
+        "DATE": dt.date.fromtimestamp,
+        "DATE_TIME": dt.datetime.fromtimestamp,
+        "TIMESTAMP": float,
+    }
+    
+    _logged = {}
+    column_names = list(sorted(types.keys(), key=len, reverse=True))
+
+    for row in data:
+        for key in row:
+            try:
+                column_name = key if key in column_names else next(c for c in column_names if c in key)
+                column_type = types[column_name]
+                python_type = TS_TO_PY_TYPES[column_type]
+
+            except (StopIteration, KeyError) as e:
+                if isinstance(e, StopIteration):
+                    msg = f"could not match column [yellow]{key}[/]"
+
+                if isinstance(e, KeyError):
+                    msg = f"could not find suitable column type for [yellow]{key}[/]=[blue]{column_type}[/]"
+
+                if key not in _logged:
+                    log.warning(f"{msg}, using VARCHAR")
+
+                _logged[key] = 1
+                python_type = str
+
+            row[key] = python_type(row[key])
+
+    return data
 
 
 class SearchMiddleware:
@@ -98,12 +149,12 @@ class SearchMiddleware:
         """
         if (worksheet, table, view).count(None) == 3:
             raise TypeError(
-                "ThoughtSpot.search() missing 1 of the required keyword-only "
+                "ThoughtSpot.data.search() missing 1 of the required keyword-only "
                 "arguments: 'worksheet', 'table', 'view'"
             )
         if (worksheet, table, view).count(None) != 2:
             raise TypeError(
-                "ThoughtSpot.search() got multiple values for one of the "
+                "ThoughtSpot.data.search() got multiple values for one of the "
                 "mutually-exclusive keyword-only arguments: 'worksheet', 'table', 'view'"
             )
 
@@ -148,17 +199,31 @@ class SearchMiddleware:
         data = []
 
         while True:
-            r = self.ts._rest_api.data.searchdata(
-                    query_string=query,
-                    data_source_guid=guid,
-                    formattype='COMPACT',
-                    batchsize=sample,
-                    offset=offset
+            try:
+                r = self.ts._rest_api.data.searchdata(
+                        query_string=query,
+                        data_source_guid=guid,
+                        formattype='COMPACT',
+                        batchsize=sample,
+                        offset=offset
+                    )
+            except httpx.HTTPStatusError as e:
+                log.debug(e, exc_info=True)
+                err = e.response.json()
+                errors = [msg for msg in json.loads(err["debug"]) if msg]
+                query = query.replace('[', r'\[')
+                raise CSToolsError(
+                    error="\n".join(errors),
+                    mitigation=(
+                        f"Double check, does this query apply to [blue]{worksheet or table or view}[/]?"
+                        f"\n\nSearch terms\n[blue]{query}"
+                    )
                 )
+
             d = r.json()
             data.extend(d.pop('data'))
             offset += d['rowCount']
-            
+
             if d['rowCount'] < d['pageSize']:
                 break
 
@@ -172,4 +237,11 @@ class SearchMiddleware:
                     f'extracting directly from the underlying data source instead!'
                 )
 
-        return [dict(zip(d['columnNames'], _clean_datetime(row))) for row in data]
+        # Get the data types
+        r = self.ts._rest_api.metadata.details(type="LOGICAL_TABLE", id=[guid])
+        data_types = {c["header"]["name"]: c["dataType"] for c in r.json()["storables"][0]["columns"]}
+
+        # Cleanups
+        data = _to_records(columns=d["columnNames"], rows=data)
+        data = _cast(data, types=data_types)
+        return data
--- cs_tools/cli/tools/extractor/app.py
+++ cs_tools/cli/tools/extractor/app.py
@@ -1,25 +1,56 @@
+from typing import Any, Dict
+import datetime as dt
 import logging
+import re
 
 from typer import Argument as A_, Option as O_  # noqa
+import sqlalchemy as sa
 import typer
 
 from cs_tools.cli.dependencies import thoughtspot
 from cs_tools.cli.types import SyncerProtocolType
-from cs_tools.cli.ux import console, CSToolsApp, CSToolsGroup
+from cs_tools.cli.ux import console, CSToolsApp
 
 from .enums import RecordsetType
 
 
 log = logging.getLogger(__name__)
+RE_LETTERS_ONLY = re.compile(r'[^A-Za-z]')
 
 
-app = CSToolsApp(
-    help="""
-    Extract data from a worksheet, view, or table in your platform.
-    """,
-    cls=CSToolsGroup,
-    options_metavar='[--version, --help]'
-)
+def infer_schema_from_results(data: Dict[str, Any], tablename: str, metadata: sa.Table) -> sa.Table:
+    """
+    """
+    PY_TO_SQL_MAPPING_TYPES = {
+        str: sa.String,
+        bool: sa.Boolean,
+        float: sa.Float,
+        int: sa.Integer,
+        dt.date: sa.Date,
+        dt.datetime: sa.DateTime,
+    }
+
+    columns = []
+
+    for key in data[0].keys():
+        max_val = max(row[key] for row in data)
+        column_name = RE_LETTERS_ONLY.sub("_", key).lower()
+        column_type = PY_TO_SQL_MAPPING_TYPES.get(type(max_val), sa.String)
+
+        if column_type == sa.Float:
+            p, _, s = str(max_val).partition(".")
+            column_type = column_type(precision=len(p) + len(s))
+
+        if column_type == sa.Integer and max_val > (2 ** 31 - 1):
+            column_type = sa.BigInteger
+
+        column = sa.Column(column_name, column_type)
+        columns.append(column)
+
+    return sa.Table(tablename, metadata, *columns)
+
+
+app = CSToolsApp(help="Extract data from a worksheet, view, or table in your platform.")
 
 
 @app.command(dependencies=[thoughtspot])
@@ -27,12 +58,12 @@ def search(
     ctx: typer.Context,
     query: str = O_(..., help='search terms to issue against the dataset'),
     dataset: str = O_(..., help='name of the worksheet, view, or table to search against'),
-    # syncer: str = O_(
-    #     ...,
-    #     help='protocol and path for options to pass to the syncer',
-    #     metavar='protocol://DEFINITION.toml',
-    #     callback=lambda ctx, to: SyncerProtocolType().convert(to, ctx=ctx)
-    # ),
+    syncer: str = O_(
+        ...,
+        help='protocol and path for options to pass to the syncer',
+        metavar='protocol://DEFINITION.toml',
+        callback=lambda ctx, to: SyncerProtocolType().convert(to, ctx=ctx)
+    ),
     target: str = O_(..., help='syncer directive to load data to'),
     data_type: RecordsetType = O_('worksheet', help='type of object to search')
 ):
@@ -42,10 +73,23 @@ def search(
     Columns must be surrounded by square brackets and fully enclosed by quotes.
     Search-level formulas are not currently supported, but a formula defined as
     part of a data source is.
+
+    If the syncer target is a database table and does not exist, we'll create it.
     """
-    ts = ctx.obj.thoughtspot
-
     with console.status(f'[bold green]retrieving data from {data_type.value} "{dataset}"..[/]'):
-        data = ts.search(query, **{data_type.value: dataset})
+        data = ctx.obj.thoughtspot.search(query, **{data_type.value: dataset})
 
-    # syncer.dump(target, data=data)
+    if not data:
+        query = query.replace("[", r"\[")
+        console.log(f"[red]No data returned from query:[/] {query}")
+        return
+
+    if hasattr(syncer, "__is_database__"):
+        table = infer_schema_from_results(data, tablename=target, metadata=syncer.metadata)
+        syncer.metadata.create_all(bind=syncer.cnxn, tables=[table])
+
+        # fix data for INSERT
+        column_names = [c.name for c in table.columns]
+        data = [dict(zip(column_names, row.values())) for row in data]
+
+    syncer.dump(target, data=data)
