6d60e6653e389048271b1f1cce38a363f26c011a
boonhapus <nicholas.cooper@thoughtspot.com>
2022-11-13 20:54:07 -0600

    add snakeviz for performance profiling

---
+++ tests/_perf.py
@@ -0,0 +1,22 @@
+import cs_tools
+
+
+if __name__ == '__main__':
+    import cProfile
+    import snakeviz
+    import pathlib
+    import sys
+
+    o = pathlib.Path("cs_tools.profile")
+    p = cProfile.Profile()
+    p.enable()
+
+    try:
+        cs_tools.cli.run()
+    finally:
+        p.disable()
+        p.dump_stats(o.as_posix())
+
+    # reset cli args to simply call snakeviz
+    sys.argv = ["snakeviz", o.as_posix()]
+    snakeviz.cli.main()
