103cfefb63efa2f578050dbec21408ccffaeac51
boonhapus <nicholas.cooper@thoughtspot.com>
2023-01-21 22:51:51 -0600

    move LiveTasks to its own module

---
+++ cs_tools/cli/layout.py
@@ -0,0 +1,161 @@
+from __future__ import annotations
+from dataclasses import dataclass, InitVar
+from typing import Callable, List, NewType, Tuple, Union
+
+from rich.console import Console, RenderableType
+from rich.align import Align
+from rich.table import Table
+from rich.live import Live
+from rich import box
+
+import datetime as dt
+
+
+_TaskName = NewType("_TaskName", str)
+_TaskDescription = NewType("_TaskDescription", str)
+
+
+def _default_layout(data: List[WorkTask]) -> Table:
+    table = Table(
+        width=150,
+        box=box.SIMPLE_HEAD,
+        row_styles=("dim", ""),
+        title_style="white",
+        caption_style="white",
+        show_footer=True
+    )
+
+    table.add_column("Status", justify="center", width=10)       # 4 + length of title
+    table.add_column("Started At", justify="center", width=14)   # 4 + length of title
+    table.add_column("Duration (s)", justify="right", width=16)  # 4 + length of title
+    table.add_column("Task", width=150 - 10 - 14 - 16, no_wrap=True)
+
+    for row in data:
+        table.add_row(*row.values)
+
+    return Align.center(table)
+
+
+@dataclass
+class WorkTask:
+    """
+    Represents a task to complete.
+
+    Enter the task will start it and initiate a refresh to the parent Live.
+
+    Attributes
+    ----------
+    name : str
+
+    description : str
+
+    status : str
+
+    started_at : dt.datetime
+
+    duration : int
+
+    """
+    name: str
+    description: str
+    status: str = ":popcorn:"
+    _live: InitVar[Live] = None
+
+    def __post_init__(self, rich_live: Live=None):
+        self._live = rich_live
+        self._started_at: dt.datetime = None
+        self._stopped_at: dt.datetime = None
+
+    @property
+    def started_at(self) -> dt.datetime:
+        return self._started_at
+
+    @property
+    def duration(self) -> dt.timedelta:
+        end = self._stopped_at or dt.datetime.now()
+        return end - self.started_at
+
+    def bind_display(self, rich_live: Live) -> None:
+        self._live = rich_live
+        return self
+
+    def __enter__(self):
+        self.status = ":fire:"
+        self._started_at = dt.datetime.now()
+        self._live.refresh()
+        return self
+
+    def __exit__(self, exc_type, exc, trace) -> None:
+        self.status = ":white_heavy_check_mark:" if exc is None else ":cross_mark:"
+        self._stopped_at = dt.datetime.now()
+        self._live.refresh()
+
+    @property
+    def values(self) -> Tuple[str]:
+        started_at = "" if self.started_at is None else self.started_at.strftime("%H:%M:%S")
+        duration = "" if self.started_at is None else f"{self.duration.total_seconds(): >6.2f}"
+        return self.status, started_at, duration, self.description
+
+
+class LiveTasks(Live):
+    """
+    A live renderable which can 
+
+    Attributes
+    ----------
+    work_items: list[WorkItem]
+
+    layout : callable(WorkItems) -> RenderableType
+
+    console : Console
+
+    """
+
+    def __init__(
+        self,
+        work_items: List[Union[WorkTask, Tuple[_TaskName, _TaskDescription]]],
+        layout: Callable[[List[WorkTask]], [RenderableType]] = _default_layout,
+        console: Console = None,
+    ):
+        super().__init__(console=console)
+        self.work_items = work_items
+        self.layout = layout
+
+    @property
+    def work_items(self) -> List[WorkTask]:
+        return self._work_items
+
+    @work_items.setter
+    def work_items(self, items: List[Union[WorkTask, Tuple[_TaskName, _TaskDescription]]]) -> None:
+        if not hasattr(self, "_work_items"):
+            self._work_items = []
+
+        for work_item in items:
+            if isinstance(work_item, tuple):
+                work_item = WorkTask(name=work_item[0], description=work_item[1], _live=self)
+
+            self._work_items.append(work_item)
+
+    def __getitem__(self, task_name: _TaskName) -> WorkTask:
+        for work_item in self.work_items:
+            if work_item.name == task_name:
+                return work_item
+        raise KeyError(f"no task found with name '{task_name}'")
+
+    def draw(self) -> None:
+        """
+        Draw the renderable.
+        """
+        with self._lock:
+            self._renderable = self.layout(self.work_items)
+
+    def refresh(self) -> None:
+        """
+        Draw and refresh the Live.
+        """
+        self.draw()
+        super().refresh()
+
+    def __enter__(self):
+        self.draw()
+        return super().__enter__()
--- cs_tools/cli/tools/falcon-sharding/layout.py
+++ cs_tools/cli/tools/falcon-sharding/layout.py
@@ -1,18 +1,9 @@
-from typing import Callable, List, Tuple, Union
-
-from rich.console import Console, RenderableType
 from rich.align import Align
 from rich.table import Table
-from rich.live import Live
 from rich import box
 
-from cs_tools.types import TMLAPIResponse
-from cs_tools import utils
 
-from . import types
-
-
-def build_table(data: List[TMLAPIResponse]) -> Table:
+def build_table() -> Table:
     """
     Layout for the UI.
     """
@@ -30,76 +21,3 @@ def build_table(data: List[TMLAPIResponse]) -> Table:
     table.add_column("GUID", justify="center", width=40)    # 4 + length of a guid
     table.add_column("Name", width=150 - 10 - 13 - 40, no_wrap=True)
     return Align.center(table)
-
-
-def build_task_list(data: List[types.WorkItem]) -> Table:
-    """
-    """
-    table = Table(
-        width=150,
-        box=box.SIMPLE_HEAD,
-        row_styles=("dim", ""),
-        title_style="white",
-        caption_style="white",
-        show_footer=True
-    )
-
-    table.add_column("Status", justify="center", width=10)       # 4 + length of title
-    table.add_column("Started At", justify="center", width=14)   # 4 + length of title
-    table.add_column("Duration (s)", justify="right", width=16)  # 4 + length of title
-    table.add_column("Task", width=150 - 10 - 14 - 16, no_wrap=True)
-
-    for row in data:
-        table.add_row(*row.values)
-
-    return Align.center(table)
-
-
-class LiveTaskList(Live):
-    """
-    """
-
-    def __init__(
-        self,
-        work_items: List[Union[types.WorkItem, Tuple[types.TaskName, types.TaskDescription]]],
-        layout: Callable[[], [RenderableType]],
-        console: Console
-    ):
-        super().__init__(console=console)
-        self.work_items = work_items
-        self.work_layout = layout
-
-    @property
-    def work_items(self) -> List[types.WorkItem]:
-        return self._work_items
-
-    @work_items.setter
-    def work_items(self, items: List[Union[types.WorkItem, Tuple[types.TaskName, types.TaskDescription]]]) -> None:
-        if not hasattr(self, "_work_items"):
-            self._work_items = []
-
-        for work_item in items:
-            if isinstance(work_item, tuple):
-                work_item = types.WorkItem(name=work_item[0], description=work_item[1], _live_display=self)
-
-            self._work_items.append(work_item)
-
-        print(self._work_items)
-
-    def __getitem__(self, task_name: types.TaskName) -> types.WorkItem:
-        return self.get_task(task_name)
-
-    def get_task(self, task_name: str) -> types.WorkItem:
-        return utils.find(lambda w: w.name == task_name, self.work_items)
-
-    def draw(self) -> None:
-        with self._lock:
-            self._renderable = self.work_layout(self.work_items)
-
-    def refresh(self) -> None:
-        self.draw()
-        super().refresh()
-
-    def __enter__(self):
-        self.draw()
-        return super().__enter__()
