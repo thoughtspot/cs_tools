084dc437ce4364c70c88eb09c046080e91a532b6
boonhapus <nicholas.cooper@thoughtspot.com>
2023-01-24 22:39:12 -0600

    [provisional] add a confirmation prompt with a keyboard listener

---
+++ cs_tools/cli/input.py
@@ -0,0 +1,41 @@
+from pynput.keyboard import Controller, Key, KeyCode
+from rich.prompt import PromptBase
+
+
+class ConfirmationPrompt(PromptBase):
+
+    def __init__(self, prompt: str, *, console, timeout: int = 60):
+        super().__init__(prompt, console=console, choices=["y", "N"])
+        self.timeout = timeout
+        self.waiting = False
+        self.response = None
+        self.kb = Controller()
+
+    def handle_kb_input(self, key):
+        if key == KeyCode.from_char("y"):
+            self.waiting = False
+            self.response = "y"
+
+        if key == KeyCode.from_char("n"):
+            self.waiting = False
+            self.response = "n"
+
+        self.kb.press(Key.backspace)
+
+    def ask(self) -> bool:
+        from pynput import keyboard
+        import time
+
+        with keyboard.Listener(on_press=self.handle_kb_input):
+            self.waiting = True
+            self._started_at = time.perf_counter()
+
+            while self.waiting:
+                time.sleep(0.1)
+
+                if (time.perf_counter() - self._started_at) > self.timeout:
+                    self.waiting = False
+                    self.response = "n"
+                    break
+
+        return self.response == "y"
