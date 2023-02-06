261b212333b763af9724a9d3356bcac4d752ac55
boonhapus <nicholas.cooper@thoughtspot.com>
2023-02-05 21:02:21 -0600

    monkey note

--- cs_tools/cli/_monkey.py
+++ cs_tools/cli/_monkey.py
@@ -10,6 +10,10 @@ def supersede_get_click_type(*, annotation: Any, parameter_info: typer.main.Para
     """
     Monkeypatch Typer because @tiangolo is AFK Maintainer and won't assign co-maintainers.
 
+    We are allowing typer.Argument and typer.Option to implement the custom_type keyword
+    here. This means we can inherit from click.ParamType and implement custom argument
+    conversion functionality.
+
     This is addressed in #77 - https://github.com/tiangolo/typer/issues/77
     """
     if hasattr(parameter_info, "custom_type") and parameter_info.custom_type is not None:
