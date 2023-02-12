734a03beb89d6137104d5320f236b4f9a8de9e84
boonhapus <nicholas.cooper@thoughtspot.com>
2023-02-12 11:07:12 -0600

    programmatic access progress

--- cs_tools/cli/loader.py
+++ cs_tools/cli/loader.py
@@ -137,6 +137,10 @@ class CSTool:
         if args is None:
             args = []
 
+        import click
+        from cs_tools import utils
+
+        click.get_current_context().obj = utils.State()
         runner = CliRunner()
         result = runner.invoke(app=self.lib.app, args=[command, *args], catch_exceptions=False)
         return result
