5e40a90ade8144088bdc6d9fb50abe723366fbe9
boonhapus <nicholas.cooper@thoughtspot.com>
2023-02-18 18:53:13 -0600

    move common elements to work

--- cs_tools/cli/tools/__b_swarm/work.py
+++ cs_tools/cli/tools/__b_swarm/work.py
@@ -1,7 +1,54 @@
 from typing import List
 
-from cs_tools.types import GUID, RecordsFormat
+import horde
+import typer
+
+from cs_tools.cli.input import ConfirmationPrompt
+from cs_tools.cli.ux import rich_console
 from cs_tools.errors import ContentDoesNotExist
+from cs_tools.types import GUID, RecordsFormat
+
+from . import models
+from . import zombie
+
+
+def eula() -> None:
+    """Issue a non-dismissable EULA."""
+    text = (
+        "\n"
+        ":zombie: [b yellow]Swarm is not magic![/] :mage:"
+        "\n"
+        "\nThis tool will simulate concurrency against your ThoughtSpot cluster as well as your database platform. "
+        "\nWe will issue queries as if we are legitimate User activity in your platform."
+        "\n"
+        "\n[b blue]Is this okay?"
+    )
+    eula = ConfirmationPrompt(text, console=rich_console)
+
+    if not eula.ask():
+        raise typer.Exit(0)
+
+
+def write_stats(syncer, *, stats: horde.Event) -> None:
+    """Write ThoughtSpot performance stats to a syncer."""
+    start_event = next(stat for stat in stats.memory if isinstance(stat.fired_event, horde.events.HordeInit))
+    data = [
+        {
+            "request_start_time": stat.fired_event.request_start_time,
+            "metadata_guid": stat.fired_event.guid,
+            "user_guid": stat.fired_event.user.guid,
+            "performance_run_id": int(start_event.fired_event._created_at),
+            "strategy": "random access",
+            "metadata_type": stat.fired_event.metadata_type,
+            "is_success": stat.fired_event.is_success,
+            "response_received_time": stat.fired_event.response_received_time,
+            "latency": stat.fired_event.latency.total_seconds(),
+        }
+        for stat in stats.memory
+        if isinstance(stat.fired_event, zombie.ThoughtSpotPerformanceEvent)
+        # for viz_id in stat.fired_event.requests
+    ]
+    syncer.dump("ts_performance_event", data=[models.PerformanceEvent(**stat).dict() for stat in data])
 
 
 def _find_all_users_with_access_to_worksheet(guid: GUID, thoughtspot) -> List[RecordsFormat]:
