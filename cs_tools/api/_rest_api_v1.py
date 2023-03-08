60863bef087a6df47b43c4df2a8bc62f778c45d2
boonhapus <nicholas.cooper@thoughtspot.com>
2023-03-07 22:45:51 -0600

    we're string enums now

--- cs_tools/api/_rest_api_v1.py
+++ cs_tools/api/_rest_api_v1.py
@@ -563,7 +563,7 @@ class RESTAPIv1:
             "id": dumps(guids),
             "permission": {
                 "permissions": {
-                    principal_guid: {"shareMode": access_level.value} for principal_guid, access_level in permissions.items()
+                    principal_guid: {"shareMode": access_level} for principal_guid, access_level in permissions.items()
                 }
             },
             # we don't support these options
