f984208064c2c6fdbc28fc876612eda6175b8074
boonhapus <nicholas.cooper@thoughtspot.com>
2023-03-17 22:31:40 -0500

    add thoughtspot cluster check to analytics

--- cs_tools/cli/_analytics.py
+++ cs_tools/cli/_analytics.py
@@ -8,8 +8,10 @@ from typing import Any, Dict, Optional
 import sysconfig
 import datetime as dt
 import platform
+import shutil
 import uuid
 
+from awesomeversion import AwesomeVersion
 from pydantic import validator
 from sqlmodel import SQLModel, Field
 
@@ -20,6 +22,10 @@ import cs_tools
 def get_database(where: str = "local") -> SQLite:
     """Get the local SQLite Analytics database."""
     syncer = SQLite(database_path=cs_tools.const.APP_DIR.resolve().joinpath('analytics.db'), truncate_on_load=False)
+
+    if AwesomeVersion(cs_tools.__version__) < AwesomeVersion("1.4.0"):
+        SQLModel.metadata.drop_all(bind=syncer.cnxn)
+
     SQLModel.metadata.create_all(bind=syncer.cnxn)
     return syncer
 
@@ -37,6 +43,7 @@ class RuntimeEnvironment(SQLModel, table=True):
     cs_tools_version: str = Field(default=cs_tools.__version__, primary_key=True)
     capture_dt: dt.datetime = Field(default_factory=dt.datetime.now)
     operating_system: str = Field(default_factory=platform.system)
+    is_thoughtspot_cluster: str = Field(default_factory=lambda: bool(shutil.which("tscli")))
     python_platform_tag: str = Field(default_factory=sysconfig.get_platform)
     python_version: str = Field(default_factory=platform.python_version)
     envt_company_name: Optional[str] = None
