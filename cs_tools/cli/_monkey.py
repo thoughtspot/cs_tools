6f7bd7fbbf27b39c0423c772c9bfc31a1c3139d9
boonhapus <nicholas.cooper@thoughtspot.com>
2023-04-30 09:42:35 -0500

    add our console to typer monkeypatching

--- cs_tools/cli/_monkey.py
+++ cs_tools/cli/_monkey.py
@@ -1,7 +1,13 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
 from typing import Any
 
+import cs_tools
 import typer
-import click
+
+if TYPE_CHECKING:
+    import click
+    import rich
 
 
 class _ArgumentInfo(typer.models.ArgumentInfo):
@@ -32,6 +38,7 @@ class _MonkeyPatchedTyper:
     """
 
     def __init__(self):
+        self._original_get_rich_console = typer.rich_utils._get_rich_console
         self._original_get_click_type = typer.main.get_click_type
         self._patch()
 
@@ -62,8 +69,16 @@ class _MonkeyPatchedTyper:
         passthru["show_default"] = passthru.get("show_default", default not in (..., None))
         return _OptionInfo(default=default, param_decls=param_decls, **passthru)
 
+    def override_console_with_ours(self, stderr: bool = False) -> rich.console.Console:  # noqa: ARG002
+        """
+        Patches:
+        - There's no formal interface for this, so inject our own console.
+        """
+        return cs_tools.cli.ux.rich_console
+
     def _patch(self) -> None:
         """Handle patching."""
+        typer.rich_utils._get_rich_console = self.override_console_with_ours
         typer.main.get_click_type = self.supersede_get_click_type
         typer.Argument = self.argument_with_custom_type
         typer.Option = self.option_with_custom_type
