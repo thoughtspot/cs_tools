108b5b9d02b9f9283656800f799d0d995ee533b1
boonhapus <nicholas.cooper@thoughtspot.com>
2023-05-20 14:53:11 -0500

    remove pynput for windows

--- cs_tools/cli/input.py
+++ cs_tools/cli/input.py
@@ -1,43 +1,84 @@
-from rich.prompt import PromptBase
-from pynput import keyboard
+from typing import TextIO
+import platform
 import time
 
+from rich.console import Console
+from rich.prompt import Confirm, InvalidResponse
+from rich.panel import Panel
+from rich.text import Text
 
-class ConfirmationPrompt(PromptBase):
+class ConfirmationPrompt(Confirm):
 
-    def __init__(self, prompt: str, *, console, timeout: int = 60):
+    def __init__(self, prompt: str = "", *, console: Console, with_prompt: bool = True, timeout: float = 60.0, **kw):
         super().__init__(prompt, console=console, choices=["y", "N"])
-        self.prompt_suffix = ""
+        self.prompt_suffix = " "
+        self.with_prompt = with_prompt
         self.timeout = timeout
-        self.waiting = False
-        self.response = None
-        self.kb = keyboard.Controller()
 
-    def handle_kb_input(self, key):
-        if key == keyboard.KeyCode.from_char("y"):
-            self.waiting = False
-            self.response = "y"
+    @classmethod
+    def ask(cls, *a, default = ..., stream = None, **kw):
+        _prompt = cls(*a, **kw)
+        return _prompt(default=default, stream=stream)
 
-        if key == keyboard.KeyCode.from_char("n"):
-            self.waiting = False
-            self.response = "n"
+    # def make_prompt(self, default) -> Text:
+    #     """ """
+    #     if not self.with_prompt or default == ...:
+    #         return Text("")
+    #     return Panel.fit(Text.from_markup(default))
 
-        self.kb.press(keyboard.Key.backspace)
+    def process_response(self, value: str) -> bool:
+        value = value.strip().casefold()
+        if value not in [choice.casefold() for choice in self.choices]:
+            raise InvalidResponse(self.validate_error_message)
+        return value == "y"
 
-    def ask(self, with_prompt: bool = True) -> bool:
-        with keyboard.Listener(on_press=self.handle_kb_input, suppress=True):
-            self.waiting = True
-            self._started_at = time.perf_counter()
+    def get_input(self, console: Console, prompt: Text, password: bool, stream: TextIO=None) -> str:
+        """ """
+        console.show_cursor(False)
+        console.print(prompt, end="")
+        response = self._background_keyboard_input()
+        console.show_cursor(True)
+        return response
 
-            if with_prompt:
-                self.console.print(self.make_prompt(...))
+    def _background_keyboard_input(self) -> str:
+        """ """
+        started_at = time.perf_counter()
 
-            while self.waiting:
-                time.sleep(0.1)
+        if platform.system() == "Windows":
+            import msvcrt
 
-                if (time.perf_counter() - self._started_at) > self.timeout:
-                    self.waiting = False
-                    self.response = "n"
+            while (time.perf_counter() - started_at) < self.timeout:
+                if msvcrt.kbhit():
+                    char = msvcrt.getwch()
                     break
+            else:
+                char = "N"
 
-        return self.response == "y"
+        else:
+            import termios
+            import sys
+            import tty
+
+            fd = sys.stdin.fileno()
+            old_stdin = termios.tcgetattr(fd)
+
+            try:
+                # https://manpages.debian.org/bullseye/manpages-dev/termios.3.en.html
+                tty.setraw(fd)
+                new_stdin = termios.tcgetattr(fd)
+                new_stdin[3] = new_stdin[3] & ~termios.ECHO
+                termios.tcsetattr(fd, termios.TCSADRAIN, new_stdin)
+                char = input("")
+
+            finally:
+                char = "N"
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_stdin)
+
+        print(f"GOT: {char}")
+        return char
+
+
+if __name__ == "__main__":
+    console = Console()
+    response = ConfirmationPrompt.ask(prompt="Continue", console=console, timeout=1)
+    print(response)
