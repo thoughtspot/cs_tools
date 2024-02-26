972020d9ccc11247790f9bbe36851b1fa0c4ec01
boonhapus <nicholas.cooper@thoughtspot.com>
2024-02-25 19:31:57 -0600

    ensure tests pass

--- tests/programmatic/test_as_ci.py
+++ tests/programmatic/test_as_ci.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import datetime as dt
 import logging
 import os
-import shlex
+import pathlib
 
 from cs_tools.programmatic import get_cs_tool
 from cs_tools.settings import CSToolsConfig
@@ -38,11 +38,17 @@ def test_build_config_from_envvar():
 
     conf = CSToolsConfig.from_environment()
 
+    # Unset ENVIRONMENT VARS.
+    del os.environ["CS_TOOLS_TEMP_DIR"]
+    del os.environ["CS_TOOLS_THOUGHTSPOT__URL"]
+    del os.environ["CS_TOOLS_THOUGHTSPOT__USERNAME"]
+    del os.environ["CS_TOOLS_THOUGHTSPOT__PASSWORD"]
+
     # fmt: off
     assert fake.temp_dir == conf.temp_dir == const.TEST_DATA_DIRECTORY
     assert fake.thoughtspot.url == conf.thoughtspot.url == "https://dogfood.thoughtspot.cloud" 
     assert fake.thoughtspot.username == conf.thoughtspot.username == "really-fake-service-account"
-    assert fake.thoughtspot.password == conf.thoughtspot.password == "eNorSk3MyanULS4tSC3SLU5NLkot0S0uKcrPS9ctSCwuLs8vSgEA-k4OAw=="
+    assert fake.thoughtspot.password == conf.thoughtspot.password == "eNorSk3MyanULS4tSC3SLU5NLkot0S0uKcrPS9ctSCwuLs8vSgEA-k4OAw=="  # noqa: E501
     # fmt: on
 
 
@@ -59,22 +65,22 @@ def test_integration_lib_login():
     assert thoughtspot.session_context is not None
 
 
-def test_integration_cli_searchable():
+def test_integration_cli_searchable(cleaned_generated_test_data_dir: pathlib.Path):
     """
     For running in serverless environments, we should be able to parse
     environment variables.
     """
     env = const.CST_CONFIG_DOT_ENV.as_posix()
     jan = dt.date(year=2024, month=1, day=1).strftime("%Y-%m-%d")
-    out = const.TEST_DATA_DIRECTORY.joinpath("generated").as_posix()
+    out = cleaned_generated_test_data_dir.as_posix()
 
     # fmt: off
     # cs_tools tools searchable bi-server --from-date %TODAY --to-date %TODAY --syncer csv://directory=%OUT
     result = (
         get_cs_tool("searchable")
         .invoke(
-            "bi-server", 
-            shlex.split(f"--from-date {jan} --to-date {jan} --syncer csv://directory={out} --config ENV:{env}")
+            command="bi-server",
+            arguments=f"--from-date {jan} --to-date {jan} --syncer csv://directory={out} --config ENV:{env}",
         )
     )
     # fmt: on
@@ -83,4 +89,4 @@ def test_integration_cli_searchable():
         log.warning(result.stdout)
 
     assert result.exit_code == 0
-    assert const.TEST_DATA_DIRECTORY.joinpath("ts_bi_server.csv").exists()
+    assert cleaned_generated_test_data_dir.joinpath("ts_bi_server.csv").exists()
