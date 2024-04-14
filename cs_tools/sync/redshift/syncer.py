06d553c7f9be8681c28d1d4cfccf04e81d397f4c
boonhapus <nicholas.cooper@thoughtspot.com>
2024-04-14 11:09:10 -0500

    secret instead of password, for future compatibility

--- cs_tools/sync/redshift/syncer.py
+++ cs_tools/sync/redshift/syncer.py
@@ -22,7 +22,7 @@ class Redshift(DatabaseSyncer):
     host: str
     port: int = 5439
     username: str
-    password: str
+    secret: str
     database: str
     authentication: Literal["basic"] = "basic"
 
@@ -35,9 +35,9 @@ class Redshift(DatabaseSyncer):
         host = self.host
         port = self.port
         username = self.username
-        password = self.password
+        secret = self.secret
         database = self.database
-        return f"redshift+psycopg2://{username}:{password}@{host}:{port}/{database}"
+        return f"redshift+psycopg2://{username}:{secret}@{host}:{port}/{database}"
 
     def __repr__(self):
         return f"<RedshiftSyncer to {self.host}/{self.database}>"
