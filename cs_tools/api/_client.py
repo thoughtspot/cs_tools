407445feb96b0d59ea824c583378df4d2572e3a5
boonhapus <nicholas.cooper@thoughtspot.com>
2024-06-04 07:11:53 -0500

    more extensive logging

--- cs_tools/api/_client.py
+++ cs_tools/api/_client.py
@@ -64,12 +64,18 @@ class RESTAPIClient:
         now = dt.datetime.now(tz=dt.timezone.utc)
         request.headers["cs-tools-request-start-utc-timestamp"] = now.isoformat()
 
-        log.debug(
-            f">>> [{now:%H:%M:%S}] {request.method} -> {request.url.path}"
+        log_msg = (
+            f">>> [{now:%H:%M:%S}] HTTP {request.method} -> {request.url.path}"
             f"\n\t=== HEADERS ===\n{request.headers}"
-            f"\n\t===  DATA   ===\n{_utils.obfuscate_sensitive_data(request.url.params)}"
-            f"\n",
         )
+
+        if request.url.params:
+            log_msg += f"\n\t===  PARAMS ===\n{_utils.obfuscate_sensitive_data(request.url.params)}"
+
+        if content := request.content.decode():
+            log_msg += f"\n\t===    DATA ===\n{_utils.obfuscate_sensitive_data(httpx.QueryParams(content))}"
+
+        log.debug(f"{log_msg}\n")
 
     def request(self, method: Literal["POST", "GET", "PUT", "DELETE"], url: str, **kwargs) -> httpx.Response:
         """Proxy httpx.Session base method on our client."""
