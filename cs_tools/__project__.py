c6acdb59bf1c8cc5b15db52cabcebd371103ba34
boonhapus <nicholas.cooper@thoughtspot.com>
2024-06-14 13:12:58 -0500

    beta bump version --> (1.5.8b6)

--- cs_tools/__project__.py
+++ cs_tools/__project__.py
@@ -1,4 +1,4 @@
-__version__ = "1.5.8b5"
+__version__ = "1.5.8b6"
 __docs__ = "https://thoughtspot.github.io/cs_tools/"
 __repo__ = "https://github.com/thoughtspot/cs_tools"
 __help__ = f"{__repo__}/discussions/"
