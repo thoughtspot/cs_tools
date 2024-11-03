2719aadb5853adf4094c4e76f57eef4628b7f6eb
boonhapus <nicholas.cooper@thoughtspot.com>
2024-11-03 13:54:33 -0600

    small bugfixes

--- cs_tools/sync/base.py
+++ cs_tools/sync/base.py
@@ -77,7 +77,9 @@ class SyncerManifest(_GlobalModel):
 
         for pip_requirement in self.requirements:
             log.debug(f"Processing requirement: {pip_requirement}")
-            cs_tools_venv.pip("install", f"{pip_requirement.requirement}", *pip_requirement.pip_args)
+            cs_tools_venv.pip(
+                "install", f"{pip_requirement.requirement}", *pip_requirement.pip_args, base_log_level="DEBUG"
+            )
 
         # Registration is successful, we can add it to the global now.
         _registry.add(__syncer_name__)
