1d8c770ec5effeb302427d157a8f2ac89503a237
boonhapus <nicholas.cooper@thoughtspot.com>
2025-01-20 16:47:34 -0600

    add privileges associated with 10.7 and RBAC feature

--- cs_tools/_types.py
+++ cs_tools/_types.py
@@ -76,23 +76,57 @@ SharingVisibility = Literal["SHARABLE", "NON_SHARABLE"]
 class GroupPrivilege(_compat.StrEnum):
     """Represents privileges which can be assigned to Groups or Roles."""
 
+    # BASE PRIVILEGES
     innate = "AUTHORING"
+
+    # SUPER USER PRIVILEGES
+    super_user__system_mananagement = "SYSTEMMANAGEMENT"
+
+    # HISTORICAL PRIVILEGES (prior to introduction of ROLES)
     can_administer_thoughtspot = "ADMINISTRATION"
-    can_upload_user_data = "USERDATAUPLOADING"
-    can_download_data = "DATADOWNLOADING"
-    has_developer_privilege = "DEVELOPER"
-    can_share_with_all_users = "SHAREWITHALL"
     can_manage_data = "DATAMANAGEMENT"
     can_use_experimental_features = "EXPERIMENTALFEATUREPRIVILEGE"
     can_invoke_custom_r_analysis = "RANALYSIS"
+    cannot_create_or_delete_pinboards = "DISABLE_PINBOARD_CREATION"
+    can_invoke_third_party_spotiq_analysis = "THIRDPARTY_ANALYSIS"
+
+    # :: ADMIN CONTROL
+    can_manage_orgs = "ORG_ADMINISTRATION"
+    can_manage_users = "USER_ADMINISTRATION"
+    can_manage_groups = "GROUP_ADMINISTRATION"
+    can_manage_roles = "ROLE_ADMINISTRATION"
+    can_manage_authentication = "AUTHENTICATION_ADMINISTRATION"
+    can_manage_application_settings = "APPLICATION_ADMINISTRATION"
+    can_setup_version_control = "CAN_SETUP_VERSION_CONTROL"
+    can_view_system_activities = "SYSTEM_INFO_ADMINISTRATION"
+    can_view_billing_information = "BILLING_INFO_ADMINISTRATION"
+    can_enable_or_disable_trusted_authentication = "CONTROL_TRUSTED_AUTH"
+    can_manage_tags = "TAGMANAGEMENT"
+    can_access_analyst_studio = "CAN_ACCESS_ANALYST_STUDIO"
+    can_manage_analyst_studio = "CAN_MANAGE_ANALYST_STUDIO"
+
+    # :: APPLICATION CONTROL
+    has_spotiq_privilege = "A3ANALYSIS"
+    has_developer_privilege = "DEVELOPER"
+    can_schedule_for_others = "JOBSCHEDULING"
     can_manage_sync = "SYNCMANAGEMENT"
     can_preview_thoughtspot_sage = "PREVIEW_THOUGHTSPOT_SAGE"
-    can_schedule_for_others = "JOBSCHEDULING"
-    has_spotiq_privilege = "A3ANALYSIS"
-    can_administer_and_bypass_rls = "BYPASSRLS"
-    cannot_create_or_delete_pinboards = "DISABLE_PINBOARD_CREATION"
+    can_manage_catalog = "CAN_CREATE_CATALOG"
     can_verify_liveboard = "LIVEBOARD_VERIFIER"
-    can_invoke_third_party_spotiq_analysis = "THIRDPARTY_ANALYSIS"
+    can_manage_version_control = "CAN_MANAGE_VERSION_CONTROL"
+
+    # :: OBJECT ACCESS CONTROL
+    can_share_with_all_users = "SHAREWITHALL"
+
+    # :: DATA CONTROL
+    can_upload_user_data = "USERDATAUPLOADING"
+    can_administer_and_bypass_rls = "BYPASSRLS"
+    can_manage_custom_calendars = "CAN_MANAGE_CUSTOM_CALENDAR"
+    can_create_edit_connections = "CAN_CREATE_OR_EDIT_CONNECTIONS"
+    can_manage_data_models = "CAN_MANAGE_WORKSHEET_VIEWS_TABLES"
+
+    # :: DOWNLOAD CONTROL
+    can_download_data = "DATADOWNLOADING"
 
 
 class Principal(_compat.TypedDict):
