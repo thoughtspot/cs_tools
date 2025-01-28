683c9f0e3979616d2e773e2692326ed3dab994bb
boonhapus <nicholas.cooper@thoughtspot.com>
2025-01-28 13:39:53 -0600

    beta bump --> v1.5.13b2

--- cs_tools/__project__.py
+++ cs_tools/__project__.py
@@ -1,4 +1,4 @@
-__version__ = "1.5.12"
+__version__ = "1.5.13b2"
 __docs__ = "https://thoughtspot.github.io/cs_tools/"
 __repo__ = "https://github.com/thoughtspot/cs_tools"
 __help__ = f"{__repo__}/discussions/"
