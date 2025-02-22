8d8a52957d498cffd260d539b7130ae11bffc3de
boonhapus <nicholas.cooper@thoughtspot.com>
2025-02-21 20:40:30 -0600

    screenshotter hook should use common utils

--- docs/hooks/screenshotter.py
+++ docs/hooks/screenshotter.py
@@ -1,18 +1,19 @@
-from __future__ import annotations
+from __future__ import annotations  # noqa: I001
 
 from typing import Optional
-import functools as ft
 import logging
 import pathlib
 import re
 import shutil
 
 from cs_tools import utils
-from cs_tools.cli.ux import RICH_CONSOLE, AsyncTyper
+from cs_tools.cli.ux import RICH_CONSOLE
 from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.structure.files import Files
 from mkdocs.structure.pages import Page
 from slugify import slugify
+
+import _common_hook_utils
 
 _LOG = logging.getLogger(__name__)
 
@@ -31,26 +32,23 @@ SCREENSHOT_CACHE: dict[str, pathlib.Path] = {}
 """Simple cache so we can skip re-generating screenshots during docs builds."""
 
 
-@ft.cache
-def _setup_cs_tools_cli() -> AsyncTyper:
-    """Fetch the CS Tools CLI."""
-    from cs_tools.cli import _monkey  # noqa: F401
-    from cs_tools.cli.commands import (
-        config as config_command,
-        log as log_command,
-        self as self_command,
-        tools as tools_command,
-    )
-    from cs_tools.cli.commands.main import app
+def on_config(config: MkDocsConfig) -> Optional[MkDocsConfig]:
+    """
+    The config event is the first event called on build and is run immediately
+    after the user configuration is loaded and validated. Any alterations to
+    the config should be made here.
 
-    tools_command._discover_tools()
+    Further reading:
+      https://www.mkdocs.org/dev-guide/plugins/#on_config
+    """
+    site_dir = pathlib.Path(config["site_dir"])
 
-    app.add_typer(tools_command.app)
-    app.add_typer(config_command.app)
-    app.add_typer(self_command.app)
-    app.add_typer(log_command.app)
+    # REFRESH THE RUN CACHE.
+    scrn_dir = site_dir / "generated" / "screenshots"
+    shutil.rmtree(scrn_dir, ignore_errors=True)
+    SCREENSHOT_CACHE.clear()
 
-    return app
+    return config
 
 
 def on_page_markdown(markdown: str, page: Page, config: MkDocsConfig, files: Files) -> Optional[str]:
@@ -64,12 +62,9 @@ def on_page_markdown(markdown: str, page: Page, config: MkDocsConfig, files: Fil
     """
     site_dir = pathlib.Path(config["site_dir"])
     scrn_dir = site_dir / "generated" / "screenshots"
+    scrn_dir.mkdir(parents=True, exist_ok=True)
 
-    app = _setup_cs_tools_cli()
-
-    if _REFRESH_SCREENSHOTS_DIRECTORY := (not SCREENSHOT_CACHE):
-        shutil.rmtree(scrn_dir, ignore_errors=True)
-        scrn_dir.mkdir(parents=True, exist_ok=True)
+    app = _common_hook_utils.setup_cs_tools_cli()
 
     for match in RE_BLOCK_IDENTITY.finditer(markdown):
         cs_tools_command = match.group("command").strip()
@@ -93,7 +88,7 @@ def on_page_markdown(markdown: str, page: Page, config: MkDocsConfig, files: Fil
             SCREENSHOT_CACHE[svg_name] = svg_path
 
         # CALCULATE THE RELATIVE PATH TO THE SCREENSHOTS DIRECTORY FROM THE CURRENT PAGE.
-        cd = "/".join("." if page.parent is None else ".." for _ in range(page.file.url.count("/")))
+        cd = "/".join("." if page.is_homepage else ".." for _ in range(page.file.url.count("/")))
         fp = f"{cd}/generated/screenshots/{svg_name}.svg"
 
         block_identity_command = match.string[match.start() : match.end()]
